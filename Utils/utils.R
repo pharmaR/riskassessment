af2f0c2547a6a1f31d3bf56f04c82642efa0b0b1
Robert Krajcik <robert.krajcik@biogen.com>
2020-09-23 12:08:27 -0400

    update timestamp() function to something MySQL|MariaDB would accept

--- Utils/utils.R
+++ Utils/utils.R
@@ -58,9 +58,10 @@ db_ins<-function(query){
 
 
 TimeStamp<-function(){
-  Timestamp_intial<-str_replace(Sys.time()," ", "; ")
-  Timestamp<-paste(Timestamp_intial, Sys.timezone())
-  return(Timestamp)
+  # Timestamp_intial<-str_replace(Sys.time()," ", "; ")
+  # Timestamp<-paste(Timestamp_intial, Sys.timezone())
+  return(lubridate::with_tz(Sys.time(), "UTC"))
+  # return(Timestamp)
 }
 
 GetUserName <- function() {
