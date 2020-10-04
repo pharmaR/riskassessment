d35d1aa16a29815ff8d1e8463ff932b1ab818b57
Marly Gotti <marly.cormar@biogen.com>
2020-10-04 02:50:22 -0400

    Fix apps role for RinPharma.

--- UI/login_screen.R
+++ UI/login_screen.R
@@ -34,7 +34,7 @@ output$screen <- renderUI({
                    tags$h3("Role", class = "mt-7")
                  ),
                  column(width = 12,
-                        textInput("role", NULL))  # Text input to enter the Role.
+                        disabled(textInput("role", NULL, value = "RinPharma-Participant")))  # Text input to enter the Role.
                ),
                tags$div(class = "col-md-12 proceed-div",
                         column(
