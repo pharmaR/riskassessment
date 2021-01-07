412dbbd5c581e2a14936d36b67c7a74c0b363b84
Marly Gotti <marly.cormar@biogen.com>
2021-01-07 00:31:27 -0500

    Add newly created sql to initialization procedures.

--- Utils/utils.R
+++ Utils/utils.R
@@ -20,6 +20,9 @@ create_db <- function(){
   # Queries needed to run the first time the db is created.
   queries <- c(
     "create_package_table.sql",
+    "create_metric_table.sql",
+    "initialize_metric_table.sql",
+    "create_package_metrics_table.sql",
     "create_MaintenanceMetrics_table.sql",
     "create_CommunityUsageMetrics_table.sql",
     "create_TestMetrics_table.sql",
