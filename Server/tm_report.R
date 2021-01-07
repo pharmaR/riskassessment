172a029f0305dc36a377e1c590b0d755cd4a77df
Marly Gotti <marly.cormar@biogen.com>
2021-01-07 00:38:50 -0500

    Replace references to the old table TestMetrics with the new table package_metrics.

--- Server/tm_report.R
+++ Server/tm_report.R
@@ -10,19 +10,22 @@ observeEvent(input$tabs, {
   req(input$select_pack)
   if (input$tabs == "reportPreview_tab_value") {
     if(input$select_pack != "Select"){
-    
-      values$riskmetrics_tm <-
-        db_fun(
-          paste0(
-            "SELECT * FROM TestMetrics WHERE TestMetrics.tm_id ='",
-            input$select_pack,
-            "'"
-          )
-        )
-      values$test_coverage <- c(strsplit(values$riskmetrics_tm$test_coverage,",")[[1]][1], strsplit(values$riskmetrics_tm$test_coverage,",")[[1]][2])
       
-      req(values$test_coverage)
-      if(values$test_coverage[2] == -1){ runjs( "setTimeout(function(){ addTextToGaugeSVG('test_coverage1');}, 5000);" ) }
+      package_id <- db_fun(paste0("SELECT id FROM package WHERE name = ", "'", input$select_pack, "'", ";"))
+      metric_id <- db_fun(paste0("SELECT id FROM metric WHERE class = 'code_coverage';"))
+      values$code_coverage <- db_fun(
+        paste0("SELECT value FROM package_metrics WHERE ",
+               "package_id = ", package_id,
+               " AND ",
+               "metric_id = ", metric_id,
+               ";"
+        )
+      )
+      values$code_coverage <- values$code_coverage$value
+      
+      req(values$code_coverage)
+      if(values$code_coverage == "pkg_metric_error")
+        runjs("setTimeout(function(){ addTextToGaugeSVG('test_coverage1');}, 5000);")
     }
   }
 })  # End of the observe.
@@ -37,17 +40,21 @@ output$test_coverage1 <- renderAmCharts({
   bands = data.frame(
     start = c(0, 40, 80),
     end = c(40, 80, 100),
-    color = ifelse(values$test_coverage[2] != -1, c("#ea3838", "#ffac29", "#00CC00"), c("#808080", "#808080", "#808080")),
+    color = ifelse(values$code_coverage != "pkg_metric_error",
+                   c("#ea3838", "#ffac29", "#00CC00"),
+                   c("#808080", "#808080", "#808080")),
     stringsAsFactors = FALSE
   )
   bands2 = data.frame(
     start = c(0, 40, 80),
     end = c(40, 80, 100),
-    color = ifelse(values$test_coverage[2] != -1, c("#ea3838", "#ffac29", "#00CC00"), c("#808080", "#808080", "#808080")),
+    color = ifelse(values$code_coverage != "pkg_metric_error",
+                   c("#ea3838", "#ffac29", "#00CC00"),
+                   c("#808080", "#808080", "#808080")),
     stringsAsFactors = FALSE
   )
   amAngularGauge(
-    x = as.numeric(ifelse(values$test_coverage[1] == "NA", 0, values$test_coverage[1])),
+    x = as.numeric(ifelse(values$code_coverage == "NA", 0, values$code_coverage)),
     start = 0,
     end = 100,
     bands = bands,
