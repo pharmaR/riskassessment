45c63b3ae8af040b43debac83fddb19850f6b9b0
Marly Gotti <marly.cormar@biogen.com>
2021-01-07 00:26:12 -0500

    Initialize the metric table with the known metrics.

---
+++ Utils/sql_queries/initialize_metric_table.sql
@@ -0,0 +1,11 @@
+INSERT INTO metric (name, description, class, weight) values 
+('package_has_vignettes', '', 'maintenance', 1),
+('news_is_current', '', 'maintenance', 1),
+('has_bug_reports', '', 'maintenance', 1),
+('package_has_website', '', 'maintenance', 1),
+('news_is_current', '', 'maintenance', 1),
+('has_a_package_maintainer', '', 'maintenance', 1),
+('source_code_is_public', '', 'maintenance', 1),
+('exported_objects_with_documentation', '', 'maintenance', 1),
+('status_of_last_30_reported_bugs', '', 'maintenance', 1),
+('covr_coverage', '', 'code_coverage', 1);
