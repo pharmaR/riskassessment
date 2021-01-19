23a990bf048bb082cb1842f616ae304232874e54
Robert Krajcik <robert.krajcik@biogen.com>
2021-01-19 11:46:41 -0500

    update initialize_metric_table

--- Utils/sql_queries/initialize_metric_table.sql
+++ Utils/sql_queries/initialize_metric_table.sql
@@ -1,11 +1,11 @@
 INSERT INTO metric (name, description, class, weight) values 
-('package_has_vignettes', '', 'maintenance', 1),
-('news_is_current', '', 'maintenance', 1),
-('has_bug_reports', '', 'maintenance', 1),
-('package_has_website', '', 'maintenance', 1),
-('news_is_current', '', 'maintenance', 1),
-('has_a_package_maintainer', '', 'maintenance', 1),
-('source_code_is_public', '', 'maintenance', 1),
-('exported_objects_with_documentation', '', 'maintenance', 1),
-('status_of_last_30_reported_bugs', '', 'maintenance', 1),
-('covr_coverage', '', 'test', 1);
+('news_current', 'News is current?', 'maintenance', 1),
+('has_vignettes', 'Presence of vignettes?', 'maintenance', 1),
+('has_bug_reports_url', 'Bugs publicly documented?', 'maintenance', 1),
+('bugs_status', 'Percent of last 30 bugs closed', 'maintenance', 1),
+('export_help', 'Percent of exported objects documented', 'maintenance', 1),
+('has_website', 'Associated website URL?', 'maintenance', 1),
+('has_maintainer', 'Has a maintainer?', 'maintenance', 1),
+('has_news', 'Number of discovered NEWS files', 'maintenance', 1),
+('has_source_control', 'Source code public?', 'maintenance', 1),
+('covr_coverage', 'Package unit test coverage', 'test', 1);
