d2ac4ac75dc1eddd170e95e6b322f4884e7bb355
Robert Krajcik <robert.krajcik@biogen.com>
2021-01-19 12:10:18 -0500

    update maintentance metrics sql table

--- Utils/sql_queries/create_MaintenanceMetrics_table.sql
+++ Utils/sql_queries/create_MaintenanceMetrics_table.sql
@@ -1,13 +1,9 @@
 CREATE TABLE IF NOT EXISTS MaintenanceMetrics (
    mm_id   								       CHAR NOT NULL, 
-   package_has_vignettes				    CHAR,
-   package_has_news                     CHAR,
-   news_is_current					       CHAR,
-   package_has_website				       CHAR,
-   has_bug_reports					       CHAR,
-   has_a_package_maintainer			    CHAR,
-   source_code_is_public   				 CHAR,
-   exported_objects_with_documentation  CHAR,
-   status_of_last_30_reported_bugs  	 CHAR,
+   mm_ver                        CHAR NOT NULL,
+   riskmetric_ver                CHAR,
+   riskmetric_date               CHAR,
+   mm_name                       CHAR,
+   mm_value                      CHAR,
    FOREIGN KEY(mm_id) REFERENCES package(name)
 );
