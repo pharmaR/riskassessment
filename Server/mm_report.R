4226da3548dcb84853479b182d7a3857c821eef4
Robert Krajcik <robert.krajcik@biogen.com>
2021-01-19 13:06:57 -0500

    update mm_report server and UI

--- Server/mm_report.R
+++ Server/mm_report.R
@@ -10,8 +10,8 @@ observe({
   req(input$select_pack)
   if(input$tabs == "reportPreview_tab_value"){
     if(input$select_pack != "Select"){
-    
-      values$riskmetrics_mm <-
+      
+      risk_mm <-
         db_fun(
           paste0(
             "SELECT * FROM MaintenanceMetrics WHERE MaintenanceMetrics.mm_id ='",
@@ -20,212 +20,51 @@ observe({
           )
         )  
       
-      values$package_has_vignettes <- c(strsplit(values$riskmetrics_mm$package_has_vignettes,",")[[1]][1], strsplit(values$riskmetrics_mm$package_has_vignettes,",")[[1]][2])
-      values$package_has_website <- c(strsplit(values$riskmetrics_mm$package_has_website,",")[[1]][1], strsplit(values$riskmetrics_mm$package_has_website,",")[[1]][2])
-      values$package_has_news <- c(strsplit(values$riskmetrics_mm$package_has_news,",")[[1]][1], strsplit(values$riskmetrics_mm$package_has_news,",")[[1]][2])
-      values$news_is_current <- c(strsplit(values$riskmetrics_mm$news_is_current,",")[[1]][1], strsplit(values$riskmetrics_mm$news_is_current,",")[[1]][2])
-      values$has_bug_reports <- c(strsplit(values$riskmetrics_mm$has_bug_reports,",")[[1]][1], strsplit(values$riskmetrics_mm$has_bug_reports,",")[[1]][2])
-      values$status_of_last_30_reported_bugs <- c(strsplit(values$riskmetrics_mm$status_of_last_30_reported_bugs,",")[[1]][1], strsplit(values$riskmetrics_mm$status_of_last_30_reported_bugs,",")[[1]][2])
-      values$exported_objects_with_documentation <- c(strsplit(values$riskmetrics_mm$exported_objects_with_documentation,",")[[1]][1], strsplit(values$riskmetrics_mm$exported_objects_with_documentation,",")[[1]][2])
-      values$source_code_is_public <- c(strsplit(values$riskmetrics_mm$source_code_is_public,",")[[1]][1], strsplit(values$riskmetrics_mm$source_code_is_public,",")[[1]][2])
-      values$has_a_package_maintainer <- c(strsplit(values$riskmetrics_mm$has_a_package_maintainer,",")[[1]][1], strsplit(values$riskmetrics_mm$has_a_package_maintainer,",")[[1]][2])
-    
-      runjs("setTimeout(function(){ capturingSizeOfInfoBoxes(); }, 500);")
-      req(values$riskmetrics_mm)
-      if(values$package_has_vignettes[2] == -1){ runjs( "setTimeout(function(){ updateInfoBoxesWhenNA('vignette1');}, 3000);" ) }
-      if(values$package_has_website[2] == -1){ runjs( "setTimeout(function(){ updateInfoBoxesWhenNA('website1');}, 3000);" ) }
-      if(values$package_has_news[2] == -1){ runjs( "setTimeout(function(){ updateInfoBoxesWhenNA('hasnews1');}, 3000);" ) }
-      if(values$news_is_current[2] == -1){ runjs( "setTimeout(function(){ updateInfoBoxesWhenNA('newscurrent1');}, 3000);" ) }
-      if(values$has_bug_reports[2] == -1){ runjs( "setTimeout(function(){ updateInfoBoxesWhenNA('bugtrack1');}, 3000);" ) }
-      if(values$status_of_last_30_reported_bugs[2] == -1){ runjs( "setTimeout(function(){ updateInfoBoxesColorWhenNA('bugstatus1');}, 3000);" ) }
-      if(values$exported_objects_with_documentation[2] == -1){ runjs( "setTimeout(function(){ updateInfoBoxesColorWhenNA('exporthelp1');}, 3000);" ) }
-      if(values$source_code_is_public[2] == -1){ runjs( "setTimeout(function(){ updateInfoBoxesWhenNA('source_pub1');}, 3000);" ) }
-      if(values$has_a_package_maintainer[2] == -1){ runjs( "setTimeout(function(){ updateInfoBoxesWhenNA('pack_maint1');}, 3000);" ) }
+      metrics_to_read <- file.path("Data", "maint_metrics_labels.csv")
+      maint_tbl <- readr::read_csv(metrics_to_read, col_types = cols(.default = "c", is_thumb = "l"))
+      
+      # add labels
+      values$risk_mm <- left_join(risk_mm, maint_tbl, by = "mm_name")
+
     }
   }
 })  # End of the observe.
-
 # End of the observe's'
 
 # Start of the render Output's'
 
-# 1. Render Output Info box to show the information on VIGNETTE Content.
+output$myboxes1 <- renderUI({
+  boxes <- list()
+  vals <- map(values$risk_mm$mm_value, ~unlist(stringr::str_split(.x, ",",2)))
+  
+  for (i in 1:nrow(values$risk_mm)){
+    if (values$risk_mm$is_thumb[[i]] == FALSE) {
+      # if NA set to -1 for function to work properly
+      vals[[i]][2] <- ifelse(vals[[i]][2] == "NA", -1, vals[[i]][2])
+      # convert proportion to percentage
+      vals[[i]][1] <- ifelse(vals[[i]][1] == "NA", NA, format(round(as.numeric(vals[[i]][1]) * 100, 2)))
+      boxes[[i]] <- info_percnt(values$risk_mm$mm_label[[i]], vals[[i]], 
+                                eval(ifelse(vals[[i]][2] == -1, "Not Applicable",
+                                            ifelse(is.na(values$risk_mm$mm_detail[[i]]),
+                                                   paste("Percentage of",tolower(values$risk_mm$mm_name[[i]])),
+                                                   values$risk_mm$mm_detail[[i]]))) )
+    } else {
+      # if we have a c() of text strings, take the first one.
+      text <- str_split(vals[[i]][2],",")[[1]][1]
+      text <- gsub('[\"]', '', text)
+      text <- ifelse(str_detect(text,"[(]"),str_sub(text,3,nchar(text)),text)
+      boxes[[i]] <- info_thumb(values$risk_mm$mm_label[[i]], vals[[i]], 
+                               eval(ifelse(vals[[i]][2] == -1, "Not Applicable", 
+                                           ifelse(vals[[i]][1] == 1, 
+                                                  ifelse(is.na(values$risk_mm$mm_detail[[i]]), text, 
+                                                         values$risk_mm$mm_detail[[i]]),
+                                                  "Nothing to see here.")))) 
+    }
+  }
+  boxes
+})
 
-output$vignette1 <- renderInfoBox({
-  req(values$package_has_vignettes)
-  infoBox(
-    title = "Presence of vignettes?",
-    if(values$package_has_vignettes[1] == 1){"YES"}
-    else if(values$package_has_vignettes[2] == -1){"NA"}
-    else{"NO"},
-    width = 3,
-    if(values$package_has_vignettes[2] == -1){"Metric is not applicable for this source of package"}
-    else{paste("The package has", values$package_has_vignettes[2], "Vignettes")},
-    icon = icon(
-      ifelse(values$package_has_vignettes[1] == 1, "thumbs-up", "thumbs-down"),
-      lib = "glyphicon"
-    ),
-    color = ifelse(values$package_has_vignettes[1] == 1, "green", "red"),
-    fill = TRUE
-  )
-})  # End of the render Output.
-
-# 2. Render Output Info box to show the information on Package Has Website.
-
-output$website1 <- renderInfoBox({
-  req(values$package_has_website)
-  infoBox(
-    title = "Associated website URL?",
-    if(values$package_has_website[1] == 1){"YES"}
-    else if(values$package_has_website[2] == -1){"NA"}
-    else{"NO"},
-    width = 3,
-    if(values$package_has_website[2] == -1){"Metric is not applicable for this source of package"}
-    else{ ifelse(values$package_has_website[1] == 1, paste("Website:",values$package_has_website[2]), "The package does not have an associated website URL")},
-    icon = icon(
-      ifelse(values$package_has_website[1] == 1, "thumbs-up", "thumbs-down"),
-      lib = "glyphicon"
-    ),
-    color = ifelse(values$package_has_website[1] == 1, "green", "red"),
-    fill = TRUE
-  )
-})  # End of the render Output.
-# 3. Render Output Info box to show the Package Has News? Content.
-
-output$hasnews1 <- renderInfoBox({
-  req(values$package_has_news)
-  infoBox(
-    title = "NEWS?",
-    if(values$package_has_news[1] == 1){"YES"}
-    else if(values$package_has_news[2] == -1){"NA"}
-    else{"NO"},
-    width = 3,
-    if(values$package_has_news[2] == -1){"Metric is not applicable for this source of package"}
-    else{ ifelse(values$package_has_news[1] == 1, "The package has a NEWS file.", "The package does not have a NEWS file")},
-    icon = icon(
-      ifelse(values$package_has_news[1] == 1, "thumbs-up", "thumbs-down"),
-      lib = "glyphicon"
-    ),
-    color = ifelse(values$package_has_news[1] == 1, "green", "red"),
-    fill = TRUE
-  )
-})  # End of the render Output.
-# 4. Render Output Info box to show the information for News is Current?
-
-output$newscurrent1 <- renderInfoBox({
-  req(values$news_is_current)
-  infoBox(
-    title = "News is current?",
-    if(values$news_is_current[1] == 1){"YES"}
-    else if(values$news_is_current[2] == -1){"NA"}
-    else{"NO"},
-    width = 3,
-    if(values$news_is_current[2] == -1){"Metric is not applicable for this source of package"}
-    else{ ifelse(values$news_is_current[1] == 1, "NEWS file contains entry for current version number", "NEWS file does not contains entry for current version number")},
-    icon = icon(
-      ifelse(values$news_is_current[1] == 1, "thumbs-up", "thumbs-down"),
-      lib = "glyphicon"
-    ),
-    color = ifelse(values$news_is_current[1] == 1, "green", "red"),
-    fill = TRUE
-  )
-})  # End of the render Output.
-
-# 5. Render Output  Info box to show the information for Does the package have Bug Report?
-
-output$bugtrack1 <- renderInfoBox({
-  req(values$has_bug_reports)
-  infoBox(
-    title = "Bugs publicly documented?",
-    if(values$has_bug_reports[1] == 1){"YES"}
-    else if(values$has_bug_reports[2] == -1){"NA"}
-    else{"NO"},
-    width = 3,
-    if(values$has_bug_reports[2] == -1){"Metric is not applicable for this source of package"}
-    else{ ifelse(values$has_bug_reports[1] == 1, paste("Bug reports URL:", values$has_bug_reports[2]), "The Bugs are not publicly documented")},
-    icon = icon(
-      ifelse(values$has_bug_reports[1] == 1, "thumbs-up", "thumbs-down"),
-      lib = "glyphicon"
-    ),
-    color = ifelse(values$has_bug_reports[1] == 1, "green", "red"),
-    fill = TRUE
-  )
-})  # End of the render Output.
-
-# 6. Render Output Info box to show the information on Bugs Status.
-
-output$bugstatus1 <- renderInfoBox({
-  req(values$status_of_last_30_reported_bugs)
-  infoBox(
-    title = "Bug closure",
-    if(values$status_of_last_30_reported_bugs[2] == -1){"NA"}
-    else{paste0(values$status_of_last_30_reported_bugs[1],"%")},
-    subtitle = if(values$status_of_last_30_reported_bugs[2] == -1){"Metric is not applicable for this source of package"}
-    else{"Percentage of last 30 bugs closed"},
-    width = 3,
-    fill = TRUE
-  )
-})  # End of the render Output.
-
-# 7. Render Output Info box to show the information on Export help.
-
-output$exporthelp1 <- renderInfoBox({
-  req(values$exported_objects_with_documentation)
-  infoBox(
-    title = "Documentation",
-    if(values$exported_objects_with_documentation[2] == -1){"NA"}
-    else{paste0(values$exported_objects_with_documentation[1],"%")},
-    subtitle = if(values$exported_objects_with_documentation[2] == -1){"Metric is not applicable for this source of package"}
-    else{"Proportion of exported objects documented"},
-    width = 3,
-    fill = TRUE
-  )
-}) # End of the render Output.
-
-# 8. Render Output Info box to show the information on source code is public?.
-
-output$source_pub1 <- renderInfoBox({
-  req(values$source_code_is_public)
-  infoBox(
-    title = "Source code public?",
-    if(values$source_code_is_public[1] == 1){"YES"}
-    else if(values$source_code_is_public[2] == -1){"NA"}
-    else{"NO"},
-    width = 3,
-    if(values$source_code_is_public[2] == -1){"Metric is not applicable for this soucre of package"}
-    else{ ifelse(values$source_code_is_public[1] == 1, paste("Source code URL:", values$source_code_is_public[2]), "Package does not have a Source code URL")},
-    icon = icon(
-      ifelse(values$source_code_is_public[1] == 1, "thumbs-up", "thumbs-down"),
-      lib = "glyphicon"
-    ),
-    color = ifelse(values$source_code_is_public[1] == 1, "green", "red"),
-    fill = TRUE
-  )
-})  # End of the Render Output.
-
-# 9. Render Output Info box to show the information on Has a package maintainer?.
-
-output$pack_maint1 <- renderInfoBox({
-  req(values$has_a_package_maintainer)
-  infoBox(
-    title = "Has a maintainer?",
-    if(values$has_a_package_maintainer[1] == 1){"YES"}
-    else if(values$has_a_package_maintainer[2] == -1){"NA"}
-    else{"NO"},
-    width = 3,
-    if(values$has_a_package_maintainer[2] == -1){"Metric is not applicable for this soucre of package"}
-    else{ ifelse(values$has_a_package_maintainer[1] == 1, values$has_a_package_maintainer[2], "Package does not have a Maintainer")},
-    icon = icon(
-      ifelse(
-        values$has_a_package_maintainer[1] == 1, "thumbs-up", "thumbs-down"),
-      lib = "glyphicon"
-    ),
-    color = ifelse(values$has_a_package_maintainer[1] == 1, "green", "red"),
-    fill = TRUE
-  )
-})  # End of the render Output.
-
-# 10. Render Output to show the comments on the application.
+# Render Output to show the comments on the application.
 
 output$mm_commented1 <- renderText({
   if (values$mm_comment_submitted == "yes" ||
--- UI/mm_report.R
+++ UI/mm_report.R
@@ -11,23 +11,8 @@ fluidRow(
   class = "mm-main-row text-center m-0",
   h3(tags$b("Maintenance Metrics"), class = "text-left"),
   fluidRow(
-    class = "mm-row-1",
-    infoBoxOutput("vignette1"),  # Info box to show the information on VIGNETTE Content.
-    infoBoxOutput("website1"),  # Info box to show the information on Package Has Website.
-    infoBoxOutput("hasnews1"),  # Info box to show the Package Has News? Content.
-  ),
-  fluidRow(
-    class = "mm-row-2",
-    infoBoxOutput("newscurrent1"),  # Info box to show the information for News is Current?
-    infoBoxOutput("bugtrack1"),  # Info box to show the information for Does the package have Bug Report?
-    infoBoxOutput("bugstatus1"),  # Info box to show the information on Bugs Status.
-  ),
-  fluidRow(
-    class = "mm-row-3",
-    infoBoxOutput("exporthelp1"),  # Info box to show the information on Export help.
-    infoBoxOutput("source_pub1"),  # Info box to show the information on source code is public?
-    infoBoxOutput("pack_maint1"),  # Info box to show the information on Has a package maintainer?
-  ),
+    uiOutput("myboxes1")
+  ), 
   fluidRow(
     class = "mm-row-comments",
     column(
