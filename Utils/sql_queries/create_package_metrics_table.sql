e2fba0e51be778d2fbdb64fded7afbf33d91e958
Marly Gotti <marly.cormar@biogen.com>
2021-01-25 20:51:16 -0500

    Provide comments on the sql query to create package_metrics db.

--- Utils/sql_queries/create_package_metrics_table.sql
+++ Utils/sql_queries/create_package_metrics_table.sql
@@ -2,7 +2,9 @@ CREATE TABLE IF NOT EXISTS package_metrics(
    id           INTEGER PRIMARY KEY AUTOINCREMENT,
    package_id   INT, 
    metric_id    INT,
-   value        CHAR, /*a value equal to 'pkg_metric_error' indicates an error*/
+   value        CHAR,
+   /* value == 'pkg_metric_error' indicates an error. */
+   /* value == NA indicates metric is not applicable for this package. */
    weight       INT,
    FOREIGN KEY (package_id) REFERENCES package(id),
    FOREIGN KEY (metric_id) REFERENCES metric(id)
