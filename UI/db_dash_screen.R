4a2e5010105a4272b04d66e55c7d2db33015c61e
Marly Gotti <marly.cormar@biogen.com>
2021-02-15 22:04:29 -0500

    Modify the UI of the db dashboard.

--- UI/db_dash_screen.R
+++ UI/db_dash_screen.R
@@ -8,48 +8,36 @@
 output$screen <- renderUI({
   fluidPage(
     fluidRow(
-      br(), br(), br(), br(),
-      column(2, ""),
-      column(8,
-             tags$style(".back2dash-icon {float: left;}"),
-             actionButton("back2dash", strong("Return to Package Dashboard"),
+      column(8, offset = 2, tags$style(".back2dash-icon {float: left;}"),
+             actionButton("back2dash", strong("Return to Dashboard"),
                           icon = icon("arrow-circle-left"))),
-      column(2, "")
     ),
-    fluidRow(
-      column(2, ""),
-      column(
-        align = "center",
-        width = 8,
-        tags$h2("Package Upload History",
-                class = "card-title text-center txt-color font-weight-bold"),
-        tags$hr(class = "bg-color"),
-        id = "db_dash_screen",
-        tags$section(
-          br(),
-          fluidRow(
-            class = "float-right r_p_format_row",
+    fluidRow(column(
+      width = 8, offset = 2, align = "center",
+      br(),
+      h2("Packages Uploaded",
+         class = "card-title text-center txt-color font-weight-bold"),
+      hr(class = "bg-color"),
+      tags$section(
+        br(), br(), DT::dataTableOutput("db_pkgs"), br(),
+        fluidRow(
+          column(
+            width = 6,
             tags$div(
-              class = "col-sm W-40 text-left float-right",
-              # Select report format.
-              selectInput("report_formats",
-                          "Select Format",
-                          c("html", "docx"))
-            ),
-            tags$div(
-              class = "col-sm float-right",
-              # Download button to export the report.
+              style = "float: right",
+              # Download button to export report(s).
               downloadButton("dwnld_sel_db_pkgs_btn",
-                             "Dwnld Report for Selection(s)",
+                             "Download Report(s)",
                              class = "download_report_btn_class btn-secondary")
+            )),
+          column(
+            width = 6,
+            tags$div(
+              style = "float: left; width: 150px",
+              # Select report format.
+              selectInput("report_formats", "Select Format", c("html", "docx"))
             )
-          ),
-          br(),
-          DT::dataTableOutput("db_pkgs"),
-          br()
-        )
-      ),
-      column(2, "")
-    )
+          )
+        ))))
   )
 })
