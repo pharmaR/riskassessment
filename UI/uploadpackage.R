21d2802f69ea1fe3bccd33aea8d576fb062e811f
Marly Gotti <marly.cormar@biogen.com>
2021-02-16 02:54:56 -0500

    Restructure upload package tab.

--- UI/uploadpackage.R
+++ UI/uploadpackage.R
@@ -11,11 +11,12 @@
 # Render Output UI for upload package.
 output$upload_package <- renderUI({
   fluidRow(
+    style = "padding-left: 30px; padding-right: 30px; padding-bottom: 50px",
     class = "u_p_main_row",
     tags$div(class = "row col-sm-12 u_p_heading_row",
              tags$h2("Upload list of R Packages")),
     fluidRow(
-      style = "text-align: left",
+      style = "text-align: left;",
       column(
         width = 4,
         introBox(
@@ -38,13 +39,15 @@ output$upload_package <- renderUI({
                      class = "sample_dataset_link"),
           data.step = 3,
           data.intro = "You can use this sample dataset to explore the app"
-        ),
-
-        introBox(
-          actionButton("help", "Press for instructions"),
-          data.step = 1,
-          data.intro = "Click here anytime you need help"
         )
+      ),
+      column(width = 8, align = "right",
+             br(), br(), br(),
+             introBox(
+               actionButton("help", "Need help?"),
+               data.step = 1,
+               data.intro = "Click here anytime you need help"
+             )
       )
     ),
     
@@ -52,6 +55,7 @@ output$upload_package <- renderUI({
     htmlOutput("upload_summary_text"),
     
     fluidRow(
+      style = "padding-left: 50px; padding-bottom: 10px",
       column(width = 6,
              div(class = "row col-sm-12 mb-4 u_p_dropdown_row",
                  uiOutput("upload_summary_select")),
