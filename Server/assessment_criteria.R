14361d16f585f98b780b62a3241c4e2f5f792952
Aaron Clark <aaron.clark@biogen.com>
2021-05-26 14:31:18 -0400

    suppress messages from assessment_criteria modal and show up to 15 records on all tabs per Robert

--- Server/assessment_criteria.R
+++ Server/assessment_criteria.R
@@ -40,7 +40,7 @@ output$riskcalc_weights_table <- DT::renderDataTable({
     options = list(
       searching = FALSE,
       lengthChange = FALSE,
-      pageLength = 10,
+      pageLength = 15,
       columnDefs = list(list(className = 'dt-center', targets = 1:2))
     )
   )
@@ -66,14 +66,14 @@ output$maintenance_desc <- renderUI({
 # Render table for Maintenance Metrics.
 output$maintenance_table <- DT::renderDataTable(
   datatable(
-    read_csv(file.path("Data", "maintenance.csv")),
+    suppressMessages(read_csv(file.path("Data", "maintenance.csv"))),
     escape = FALSE,
     class = "cell-border",
     selection = 'none',
     options = list(
       sScrollX = "100%",
       aLengthMenu = list(c(5, 10, 20, 100,-1), list('5', '10', '20', '100', 'All')),
-      iDisplayLength = 5
+      iDisplayLength = 15
     )
   )
 )
@@ -89,14 +89,14 @@ output$community_usage_desc <- renderText({
 # Render table for Community Usage Metrics.
 output$community_usage_table <- DT::renderDataTable(
   datatable(
-    read_csv(file.path("Data", "community.csv")),
+    suppressMessages(read_csv(file.path("Data", "community.csv"))),
     escape = FALSE,
     class = "cell-border",
     selection = 'none',
     options = list(
       sScrollX = "100%",
       aLengthMenu = list(c(5, 10, 20, 100,-1), list('5', '10', '20', '100', 'All')),
-      iDisplayLength = 5
+      iDisplayLength = 15
     )
   )
 )
