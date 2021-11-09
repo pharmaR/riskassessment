fba54859e526427478869ef0482109bcad0fa0b1
Marly Gotti <marlygotti@gmail.com>
2021-11-09 11:15:10 -0500

    Improve app sidebar ui.

--- UI/dashboard_screen.R
+++ UI/dashboard_screen.R
@@ -28,18 +28,18 @@ output$screen <- renderUI({
                    uiOutput("sel_pack"), # UI for select package.
                    #uiOutput("sel_ver"), # UI for version of the selected package.
                    # uiOutput("status"), # Display the status of the package.
-                   fixedRow(
+                   fluidRow(
                      column(6, wellPanel(
                        id = "diyValBoxStatus",
-                       style = "height: 150px; border-radius: 25px; border-style: dotted; background-color: #C0C0C0 !important; color: #FFFFFF !important;",
-                       uiOutput("status"), # Display the score of the package.
-                       h3("Status")
+                       style = "height: 150px; border-radius: 10px; border-style: dotted; background-color: #C0C0C0 !important; color: #FFFFFF !important;",
+                       h3("Status"),
+                       uiOutput("status") # Display the score of the package.
                      )),
                      column(6, wellPanel(
                        id = "diyValBoxScore",
-                       style = "height: 150px; border-radius: 25px; border-style: dotted; background-color: #1E90FF !important; color: #FFFFFF !important;",
-                       uiOutput("score"), # Display the score of the package.
-                       h3("Risk Score")
+                       style = "height: 150px; border-radius: 10px; border-style: dotted; background-color: #C0C0C0 !important; color: #FFFFFF !important;",
+                       h3("Risk"),
+                       uiOutput("score") # Display the score of the package.
                        # shinydashboard::valueBoxOutput("score", width = 12),
                      ))
                    ),
