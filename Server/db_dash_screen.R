5cf056b9b5664da1ed142eade721f27c167c5e4f
Aaron Clark <aaron.clark@biogen.com>
2022-01-18 11:48:22 -0500

    forgot to find version for filename

--- Server/db_dash_screen.R
+++ Server/db_dash_screen.R
@@ -102,7 +102,7 @@ output$dwnld_sel_db_pkgs_btn <- downloadHandler(
         for (i in 1:n_pkgs) {
           # Grab package name and version, then create filename and path.
           this_pkg <- these_pkgs$name[i]
-          # this_ver <- these_pkgs$version[i]
+          this_ver <- these_pkgs$version[i]
           file_named <- paste0(this_pkg,"_",this_ver,"_Risk_Assessment.",input$report_formats)
           path <- file.path(my_dir, file_named)
           # Render the report, passing parameters to the rmd file.
