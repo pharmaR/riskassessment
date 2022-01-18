afcb5779ca961959aa9038d61d889e3ee7425003
Aaron Clark <aaron.clark@biogen.com>
2022-01-18 11:45:48 -0500

    Add pkg version back into filename for bulk report downloader

--- Server/db_dash_screen.R
+++ Server/db_dash_screen.R
@@ -103,7 +103,7 @@ output$dwnld_sel_db_pkgs_btn <- downloadHandler(
           # Grab package name and version, then create filename and path.
           this_pkg <- these_pkgs$name[i]
           # this_ver <- these_pkgs$version[i]
-          file_named <- paste0(this_pkg,"_Risk_Assessment.",input$report_formats)
+          file_named <- paste0(this_pkg,"_",this_ver,"_Risk_Assessment.",input$report_formats)
           path <- file.path(my_dir, file_named)
           # Render the report, passing parameters to the rmd file.
           rmarkdown::render(
