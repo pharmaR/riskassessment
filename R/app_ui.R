639822333cfc4484c8b0c544957b3460723033ab
Aaron Clark <aaron.clark@biogen.com>
2022-01-20 10:12:38 -0500

    comment out some mods

--- R/app_ui.R
+++ R/app_ui.R
@@ -13,7 +13,7 @@ app_ui <- function(request) {
     golem_add_external_resources(),
     
     # Your application UI logic 
-    shinymanager::secure_app(
+    # shinymanager::secure_app(
       shinydashboard::dashboardPage(
         title = "R Package Risk Assessment App",
         shinydashboard::dashboardHeader(
@@ -56,17 +56,17 @@ app_ui <- function(request) {
           # UI screen to load the required screen
           uiOutput("screen")
         )
-      ), 
+      )#, 
       # customize top and bottom of login screen
-      tags_top = 
-        tags$div(
-          tags$h2("Risk Assessment Application", style = "align:center"),
-          tags$img(
-            src = "logo_no_text.png",
-            width = 100, style = "align:center"
-          )),
-      enable_admin = TRUE
-    )
+    #   tags_top = 
+    #     tags$div(
+    #       tags$h2("Risk Assessment Application", style = "align:center"),
+    #       tags$img(
+    #         src = "logo_no_text.png",
+    #         width = 100, style = "align:center"
+    #       )),
+    #   enable_admin = TRUE
+    # )
   )
 }
 
