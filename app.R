401fa543199e8f456ccc2852dc9544fe4aaefb9c
Marly Gotti <marlygotti@gmail.com>
2022-01-20 15:33:20 -0500

    Add server side for db.

--- app.R
+++ app.R
@@ -167,15 +167,11 @@ ui <- shinymanager::secure_app(
 # Create Server Code.
 server <- function(session, input, output) {
   
+  # Assessment criteria information tab.
   assessmentInfoServer("assessmentInfo")
   
-  observeEvent(input$assessment_info, {
-    showModal(
-      modalDialog(
-        footer = NULL, easyClose = TRUE,
-        assessmentInfoUI("assessmentInfo")
-    ))
-  })
+  # Database view.
+  databaseViewServer("databaseView")
   
   # check_credentials directly on sqlite db
   res_auth <- secure_server(
@@ -191,7 +187,6 @@ server <- function(session, input, output) {
   
   # Load reactive values into values.
   values <- reactiveValues()
-  values$current_screen <- "dashboard_screen" 
   values$uploaded_file_status <- "no_status"
   values$upload_complete <- "upload_incomplete"
   values$select_pack <- "Select"
@@ -206,9 +201,6 @@ server <- function(session, input, output) {
     role <- ifelse(res_auth$admin == TRUE, "admin", "user")
     values$role <- trimws(role)
   })
-  
-  # Load Server Source module file of Package Review History.
-  source(file.path("Server", "db_dash_screen.R"), local = TRUE)
 
   # Load Server Source module file of Sidebar.
   source(file.path("Server", "sidebar.R"), local = TRUE)
@@ -228,23 +220,6 @@ server <- function(session, input, output) {
   # Load Source files of UI and Server modules of Community Usage Tab.
   source(file.path("UI", "communityusage_metrics.R"), local = TRUE)
   source(file.path("Server", "communityusage_metrics.R"), local = TRUE)
-  
-  # Start of the observes
-  # 1. Observe to Load Source files of UI module of selected screen (Package
-  # Dashboard, DB Dashboard, or Login Screen).
-  observeEvent(input$db_dash_bttn,{
-    values$current_screen <- "db_dash_screen"
-  })
-  
-  observeEvent(values$current_screen, {
-    if(values$current_screen == "db_dash_screen") {
-      source(file.path("UI", "db_dash_screen.R"), local = TRUE)
-      shinyjs::hide("assessment_criteria_bttn")
-    } else{
-      source(file.path("UI", "dashboard_screen.R"), local = TRUE)
-      shinyjs::show("assessment_criteria_bttn")
-    }
-  })
   
   # 2. Observe to select the package, score, decision and load the data into
   # a reactive variable.
