9073ba198152878c2eac5b724fcdc428b06af52a
Marly Gotti <marlygotti@gmail.com>
2022-01-21 14:10:57 -0500

    Reestructure sidebar.

--- R/sidebar.R
+++ R/sidebar.R
@@ -31,30 +31,30 @@ sidebarUI <- function(id) {
       ))
     ),
     
+    br(), br(),
+    
+    sliderTextInput(
+      inputId = NS(id, "decision"),
+      h5("Select Overall Risk"), 
+      selected = NULL,
+      grid = TRUE,
+      c("Low", "Medium", "High")
+    ),
+    
+    # Action button to submit decision for selected package.
+    actionButton(NS(id, "submit_decision"), "Submit Decision", width = "100%"),
+    
+    br(), br(),
+    
     textAreaInput(
       inputId = NS(id, "overall_comment"),
       h5("Select Overall Comment"),
       rows = 5,
-      placeholder = paste("Current Comment:")
+      placeholder = ""
     ),
     
     # Submit Overall Comment for selected Package.
-    actionButton(NS(id, "submit_overall_comment"), "Submit Comment"),
-    
-    div(
-      HTML("<i class='fas fa-info-circle fa-2x float-right txt-color cursor-help' title='Once submitted the decision cannot be reverted and comments in group and package level will be frozen'></i>"),
-      # Slider input to select the decision for selected package.
-      sliderTextInput(
-        inputId = NS(id, "decision"),
-        h3("Overall Risk:"),
-        selected = NULL,
-        grid = TRUE,
-        c("Low", "Medium", "High")
-      ),
-      
-      # Action button to submit decision for selected package.
-      actionButton(NS(id, "submit_decision"), "Submit Decision")
-    )
+    actionButton(NS(id, "submit_overall_comment"), "Submit Comment", width = "100%")
   )
 }
 
@@ -68,14 +68,14 @@ sidebarServer <- function(id) {
     selected_pkg <- reactive({
       req(input$select_pkg)
       req(input$select_ver)
-  
+      
       db_fun(glue(
         "SELECT name, score, decision, version
         FROM package
         WHERE name = '{input$select_pkg}'"))
     })
-
-    # Update package list (just once).
+    
+    # Update package list after loading the app.
     observeEvent(input$select_pkg, {
       updateSelectizeInput(
         inputId = "select_pkg",
@@ -109,9 +109,97 @@ sidebarServer <- function(id) {
       if(input$select_ver == "-")
         validate("Please select a version")
       
+      status <- ifelse(selected_pkg()$decision == "", "Under Review", "Reviewed")
+      h5(status)
+    })
+    
+    # Display the risk score of the selected package.
+    output$score <- renderUI({
+      req(input$select_pkg)
+      req(input$select_ver)
       
-      if(selected_pkg()$decision == "") h2("Under Review")
-      else h2("Reviewed")
+      if(input$select_pkg == "-")
+        validate("Please select a package")
+      if(input$select_ver == "-")
+        validate("Please select a version")
+      
+      req(selected_pkg())
+      
+      h5(selected_pkg()$score)
+    })
+    
+    # Display comments for selected package.
+    observeEvent(input$select_pkg, {
+      req(input$select_pkg)
+      req(input$select_ver)
+      
+      if(input$select_pkg == "-")
+        validate("Please select a package")
+      if(input$select_ver == "-")
+        validate("Please select a version")
+      
+      comments <- db_fun(glue(
+        "SELECT comment FROM comments
+          WHERE comm_id = '{input$select_pkg}'
+          AND comment_type = 'o'"))
+      
+      updateTextAreaInput(session, "overall_comment", placeholder = comments$comment)
+    })
+    
+    # Update db if comment is submitted.
+    observeEvent(input$submit_overall_comment, {
+      
+      current_comment <- trimws(input$overall_comment)
+      
+      if(current_comment == "")
+        validate("Please enter a comment.")
+      
+      previous_comments <- 
+        db_fun(glue(
+          "SELECT *
+            FROM comments
+            WHERE comment_type = 'o' AND comm_id = '{selected_pkg()$name}'"))
+      
+      if (nrow(previous_comments) != 0) {
+        showModal(modalDialog(
+          title = h2("Update Comment"),
+          h3("Do you want to update your previous comment?"),
+          br(),
+          h5(strong("Previous Comment:")),
+          h5(previous_comments$comment),
+          h5(strong("Current Comment:")),
+          h5(current_comment),
+          HTML("Yes - Overwrites the previous comment.<br>
+               Edit - Go back to editing the comment.<br>
+               No - Exits from window and removes the text in comment box."
+          ),
+          footer = tagList(
+            actionButton("submit_overall_comment_yes", "Yes", class = "btn-success"),
+            actionButton("submit_overall_comment_edit", "Edit", class = "btn-secondary"),
+            actionButton("submit_overall_comment_no", "No", class = "btn-unsuccess")
+          )
+        ))
+      } else{
+        print(glue("values('{selected_pkg()$name}', '{input$user$name}', '{input$user$rule}', '{current_comment}', 'o', '{TimeStamp()}')"))
+        db_ins(glue("INSERT INTO comments
+                    values('{selected_pkg()$name}', '{input$user$name}', '{input$user$rule}', '{current_comment}', 'o', '{TimeStamp()}')"))
+        
+        updateTextAreaInput(session, "current_comment", placeholder = paste("current comment:", current_comment))
+      }
+    })
+    
+    # Update overall comment by user's request.
+    observeEvent(input$submit_overall_comment_yes, {
+      db_ins(glue(
+        "UPDATE comments
+          SET comment = '{input$overall_comment}' added_on = '{TimeStamp()}'
+          WHERE comm_id = '{selected_pkg()$name}' AND
+          user_name = '{input$user$name}' AND
+          user_role = '{input$user$rule}' AND
+          comment_type = 'o'"
+      ))
+      
+      updateTextAreaInput(session, "overall_comment", placeholder = paste("current comment:", overall_comment))
     })
     
     # Update the sidebar if a decision was previously made.
@@ -137,33 +225,9 @@ sidebarServer <- function(id) {
         disable("submit_decision")
         disable("overall_comment")
         disable("submit_overall_comment")
-        
       }
     }, ignoreInit = TRUE)
     
-    # Display the risk score of the selected package.
-    output$score <- renderUI({
-      req(input$select_pkg)
-      
-      # Score defaults to NA.
-      score_output <- ifelse(input$select_pkg != "Select", selected_pkg()$score, "NA")
-      h1(strong(score_output))
-    })
-    
-
-    
-    observeEvent(input$select_pkg, {
-      if (trimws(input$select_pkg) != "Select" && trimws(input$select_pkg) != "") {
-
-        comments <- db_fun(glue(
-          "SELECT comment FROM Comments WHERE comm_id = '", input$select_pkg, "'
-          AND user_name = '", input$name, "'
-          AND user_role = '", input$role, "'
-          AND comment_type = 'o'"))
-        
-        updateTextAreaInput(session, "overall_comment", placeholder = paste("current comment:", comments$comment))
-      }
-    })
     
     # Show a confirmation modal when submitting a decision.
     observeEvent(input$submit_decision, {
@@ -212,93 +276,10 @@ sidebarServer <- function(id) {
       removeModal()
     })
     
-    observeEvent(input$submit_overall_comment, {
-      
-      overall_comment <- input$overall_comment
-      values$overall_comments <- trimws(overall_comment)
-      if (values$overall_comments != "") {
-        
-        comments_submitted <-
-          db_fun(
-            paste0(
-              "SELECT * FROM Comments WHERE comment_type = 'o' AND comm_id = '",
-              selected_pkg$name,
-              "'"
-            )
-          )
-        if (values$name %in% comments_submitted$user_name &&
-            values$role %in% comments_submitted$user_role) {
-          comment_submitted <-
-            filter(
-              comments_submitted,
-              comments_submitted$user_name == values$name &
-                comments_submitted$user_role == values$role
-            )
-          showModal(modalDialog(
-            title = h2("Update Comment", class = "mb-0 mt-0 txt-color"),
-            h3("Do you want to update your previous comment?", class = "mt-0"),
-            br(),
-            h4(strong("Previous Comment:")),
-            h5(comment_submitted$comment),
-            h4(strong("Current Comment:")),
-            h5(values$overall_comments),
-            HTML(
-              "<h5 class = 'mt-25 mb-0'><b>Note: </b> <br>Yes - Overwrites the previous comment.<br>Edit - Go back to editing the comment.<br>No - Exits from window and removes the text in comment box.</h5>"
-            ),
-            footer = tagList(
-              actionButton("submit_overall_comment_yes", "Yes", class = "submit_overall_comment_yes btn-success"),
-              actionButton("submit_overall_comment_edit", "Edit", class = "submit_overall_comment_yes btn-secondary"),
-              actionButton("submit_overall_comment_no", "No", class = "submit_overall_comment_no btn-unsuccess")
-            )
-          ))
-        } else{
-          db_ins(
-            paste0(
-              "INSERT INTO Comments values('", selected_pkg()$name, "',",
-              "'", values$name, "'," ,
-              "'", values$role, "',",
-              "'", values$overall_comments, "',",
-              "'o',",
-              "'", TimeStamp(), "'" ,
-              ")"
-            )
-          )
-          values$o_comment_submitted <- "yes"
-          updateTextAreaInput(session, "overall_comment", value = "")
-          updateTextAreaInput(session, "overall_comment", placeholder = paste("current comment:", values$overall_comments))
-        }
-        
-        # After comment added to Comments table, update db dash
-        values$db_pkg_overview <- update_db_dash()
-      }
-    })
-    
-    # 6. Observe Event to update overall comment.
-    observeEvent(input$submit_overall_comment_yes, {
-      db_ins(glue(
-          "UPDATE Comments
-          SET comment = '{values$overall_comments}' added_on = '{TimeStamp()}'
-          WHERE comm_id = '{selected_pkg()$name}' AND
-          user_name = '{values$name}' AND
-          user_role = '{values$role}' AND
-          comment_type = 'o'"
-        )
-      )
-
-      updateTextAreaInput(session, "overall_comment", value = "")
-      updateTextAreaInput(session, "overall_comment", placeholder = paste("current comment:", values$overall_comments))
-      removeModal()
-    })
-    
-    # Remove modal that edits overall comment.
-    observeEvent(input$submit_overall_comment_edit, {
-      removeModal()
-    })
-    
-    # Modal to remove text in comment box.
-    observeEvent(input$submit_overall_comment_no, {
-      updateTextAreaInput(session, "overall_comment", value = "")
-      removeModal()
-    })
+    # Module output values.
+    list(
+      name = reactive(input$select_pkg),
+      version = reactive(input$select_ver)
+    )
   })
 }
