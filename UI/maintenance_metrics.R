39df6d365e7233cbc5928309e2e7a31e07f9afc6
Marly Gotti <marlygotti@gmail.com>
2022-01-21 21:32:17 -0500

    Remove manual checks and use req.

--- UI/maintenance_metrics.R
+++ UI/maintenance_metrics.R
@@ -1,21 +1,20 @@
 # Render Output UI for Maintenance Metrics.
 output$maintenance_metrics <- renderUI({
-  Sys.sleep(0.1)
-  if (!is.null(values$packsDB$name) &&
-      !identical(values$packsDB$name, character(0))) {
-    if (input$select_pack != "Select") {
-      shiny::tagList(
-        br(),
-        div(class = "row col-sm-12 u_p_heading_row",
-                 actionBttn("help_mm", "Need help?", color = "primary",
-                   icon = icon("far fa-star"),
-                   block = FALSE, style = "simple", size = "sm")),
-        br(), br(),
-        fluidRow(
-          div(style = "height:25px;"),
-          class = "mm-main-row",
-          div(id = "mm_infoboxes", 
-              fluidRow(
+  req(selected_pkg$name())
+  
+  if(selected_pkg$name() == "-")
+    showSelectPackageMessage()
+  
+  else {
+    shiny::tagList(
+      br(),
+      div(addHelpButton("help_mm")),
+      br(), br(),
+      fluidRow(
+        div(style = "height:25px;"),
+        class = "mm-main-row",
+        div(id = "mm_infoboxes", 
+            fluidRow(
               class = "mm-row-1",
               infoBoxOutput("has_vignettes"),  # Info box for 'has_vignettes' metric.
               infoBoxOutput("has_website"),  # Info box for 'has_website' metric.
@@ -33,70 +32,37 @@ output$maintenance_metrics <- renderUI({
               infoBoxOutput("has_source_control"),  # Info box for 'has_source_control' metric.
               infoBoxOutput("has_maintainer"),  # Info box for 'has_maintainer' metric.
             )
-          ),
-          fluidRow(
-            id = "mm_add_comment",
-            class = "mm-row-comments-box",
-            column(
-              width = 8,
-              class = "mb-4 label-float-left",
-              # Text input box to leave the Maintenance Metrics Comments.
-              textAreaInput(
-                "mm_comment",
-                h3(tags$b("Leave Your Comment for Maintenance Metrics:")),
-                width = "100%",
-                rows = 4,
-                placeholder = paste("Commenting as", values$name, "(", values$role, ")")
-              ) %>%
-                shiny::tagAppendAttributes(style = 'width: 100%;'),
-              # Action button to submit the comment.
-              actionButton("submit_mm_comment", class = "submit_mm_comment_class btn-secondary", "Submit")
-            )
-          ),
-          fluidRow(
-            id = "mm_prev_comments",
-            class = "mm-row-comments",
-            column(
-              width = 12,
-              align = "left",
-              h3(tags$b(paste0('Comments(',nrow(values$comment_mm2),'):'))),
-              htmlOutput("mm_commented")  # html output to show the comments on application.
-            )
+        ),
+        fluidRow(
+          id = "mm_add_comment",
+          class = "mm-row-comments-box",
+          column(
+            width = 8,
+            class = "mb-4 label-float-left",
+            # Text input box to leave the Maintenance Metrics Comments.
+            textAreaInput(
+              "mm_comment",
+              h3(tags$b("Leave Your Comment for Maintenance Metrics:")),
+              width = "100%",
+              rows = 4,
+              placeholder = paste("Commenting as", values$name, "(", values$role, ")")
+            ) %>%
+              shiny::tagAppendAttributes(style = 'width: 100%;'),
+            # Action button to submit the comment.
+            actionButton("submit_mm_comment", class = "submit_mm_comment_class btn-secondary", "Submit")
+          )
+        ),
+        fluidRow(
+          id = "mm_prev_comments",
+          class = "mm-row-comments",
+          column(
+            width = 12,
+            align = "left",
+            h3(tags$b(paste0('Comments(',nrow(values$comment_mm2),'):'))),
+            htmlOutput("mm_commented")  # html output to show the comments on application.
           )
         )
-      )
-    } 
-    # Show the select the package message if user not selected any package from dropdown in the application. 
-    
-    else{
-      fluidRow(
-        div(style = "height:150px;"),
-        class = "",
-        id = "Upload_mm",
-        column(
-          width = 12,
-          align = "center",
-          class = "",
-          h1("Please select a package")
-        )
-      )
-    }
-  }
-  # Show the upload a list of R packages message if application not loaded the packages from DB.
-  
-  else{
-    fluidRow(
-      div(style = "height:150px;"),
-      class = "",
-      id = "Upload",
-      column(
-        width = 12,
-        align = "center",
-        class = "",
-        h1("Please upload a list of R packages to proceed")
       )
     )
   }
 })
-
-# End of the Maintenance_Metrics Source file for UI Module.
