b762c43678b5be6debfcb1b0b93a6e6d8824e6d1
Marly Gotti <marlygotti@gmail.com>
2022-01-21 21:33:28 -0500

    Upgrade design.

--- UI/uploadpackage.R
+++ UI/uploadpackage.R
@@ -6,35 +6,25 @@ output$upload_package <- renderUI({
     addHelpButton("help"),
     
     fluidRow(
-      style = "text-align: left;",
       column(
         width = 4,
-        tags$h4("Choose a CSV File", class = "chooseCSVtext"),
-        # file input to choose the csv from local to application.
         fileInput(
-          "uploaded_file",
-          "",
+          inputId = "uploaded_file",
+          label = "Choose a CSV file",
           accept = ".csv",
-          placeholder = "No file selected"
-        ),
-        # Click on Button to view the sample format dataset to upload.
-        actionLink("upload_format", "View Sample Dataset",
-                   class = "sample_dataset_link")
+          placeholder = "No file selected"),
+        
+        actionLink("upload_format", "View Sample Dataset")
       )
     ),
     
     # Display the summary information of the uploaded csv.
-    htmlOutput("upload_summary_text"),
+    fluidRow(column(width = 12, htmlOutput("upload_summary_text"))),
     
     fluidRow(
-      style = "padding-left: 50px; padding-bottom: 10px",
-      column(width = 6,
-             div(id = "upload_summary",
-                 div(class = "row col-sm-12 mb-4 u_p_dropdown_row",
-                     uiOutput("upload_summary_select")),
-                 # Display the table with total rows in the DB.
-                 dataTableOutput("total_new_undis_dup_table"))
-      )
+      column(width = 6, uiOutput("upload_summary_select")),
+      # Display the table with total rows in the DB.
+      column(width = 6, dataTableOutput("total_new_undis_dup_table"))
     )
   )
 })
