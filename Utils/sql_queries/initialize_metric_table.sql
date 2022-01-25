d597b27678395f62c5a8fc0f180ce4e21aa7023e
Marly Gotti <marlygotti@gmail.com>
2022-01-24 23:23:39 -0500

    Initialize metrics with title, is_perc, and is_url.

--- Utils/sql_queries/initialize_metric_table.sql
+++ Utils/sql_queries/initialize_metric_table.sql
@@ -1,13 +1,15 @@
-INSERT INTO metric (name, description, class, weight) values 
-('has_vignettes', '', 'maintenance', 1),
-('has_news', '', 'maintenance', 1),
-('has_bug_reports_url', '', 'maintenance', 1),
-('has_website', '', 'maintenance', 1),
-('news_current', '', 'maintenance', 1),
-('has_maintainer', '', 'maintenance', 1),
-('has_source_control', '', 'maintenance', 1),
-('export_help', '', 'maintenance', 1),
-('bugs_status', '', 'maintenance', 1),
-('license', '', 'maintenance', 1),
-('covr_coverage', '', 'test', 0),
-('downloads_1yr', '', 'community', 1);
+INSERT INTO metric 
+(name, long_name, description, is_perc, is_url, class, weight)
+VALUES 
+('has_vignettes',       'Vignettes',         'Number of vignettes',              0, 0, 'maintenance', 1),
+('has_news',            'NEWS file',         'Number of NEWS files',             0, 0, 'maintenance', 1),
+('news_current',        'NEWS current',      'NEWS contains current version',    0, 0, 'maintenance', 1),
+('has_bug_reports_url', 'Report Bugs',       'Public url to report bugs',        0, 1, 'maintenance', 1),
+('has_website',         'Website',           'Package public website',           0, 1, 'maintenance', 1),
+('has_maintainer',      'Maintainer',        'Package maintainers',              0, 0, 'maintenance', 1),
+('has_source_control',  'Source Control',    'Package source control url',       0, 1, 'maintenance', 1),
+('export_help',         'Documentation',     '% of exported objects documented', 1, 0, 'maintenance', 1),
+('bugs_status',         'Bugs Closure Rate', '% of the last 30 bugs closed',     1, 0, 'maintenance', 1),
+('license',             'License',           "Package's license",                0, 0, 'maintenance', 1),
+('covr_coverage',       'Test Coverage',     'Percentage of objects tested',     0, 1, 'test', 0),
+('downloads_1yr',       'Downloads',         'Number of package downloads in the last year', 0, 0, 'community', 1);
