66acc85d95652eb346a5b7aec2e70e1dc9c0e503
Marly Gotti <marlygotti@gmail.com>
2022-01-25 16:34:56 -0500

    Shorten metric title.

--- Utils/sql_queries/initialize_metric_table.sql
+++ Utils/sql_queries/initialize_metric_table.sql
@@ -8,7 +8,7 @@ VALUES
 ('has_website',         'Website',           'Package public website',           0, 1, 'maintenance', 1),
 ('has_maintainer',      'Maintainer',        'Package maintainers',              0, 0, 'maintenance', 1),
 ('has_source_control',  'Source Control',    'Package source control url',       0, 1, 'maintenance', 1),
-('export_help',         'Documentation',     '% of exported objects documented', 1, 0, 'maintenance', 1),
+('export_help',         'Documentation',     '% of documented objects',          1, 0, 'maintenance', 1),
 ('bugs_status',         'Bugs Closure Rate', '% of the last 30 bugs closed',     1, 0, 'maintenance', 1),
 ('license',             'License',           "Package's license",                0, 0, 'maintenance', 1),
 ('covr_coverage',       'Test Coverage',     'Percentage of objects tested',     0, 1, 'test', 0),
