7687794cad77234b144f65f6fd67dce71b43c555
Marly Gotti <marlygotti@gmail.com>
2022-01-29 14:39:53 -0500

    Remove columns that are not needed.

--- Utils/sql_queries/create_community_usage_metrics_table.sql
+++ Utils/sql_queries/create_community_usage_metrics_table.sql
@@ -1,9 +1,8 @@
 CREATE TABLE IF NOT EXISTS community_usage_metrics (
   cum_id 					           CHAR NOT NULL, 
-  month_year					       CHAR, /* Format: Jun 2022 */
-  no_of_downloads 			     INT,
-  ver_release 				       CHAR,
-  time_since_first_release   INT, /* Number of months since the first package release.*/
-  time_since_version_release INT, /* Number of months since last version release.*/
+  month					             INT, /* Format: 01 */
+  year					             INT, /* Format: 2022 */
+  downloads 			           INT,
+  version    				         CHAR,
   FOREIGN KEY(cum_id) REFERENCES package(name)
 );
