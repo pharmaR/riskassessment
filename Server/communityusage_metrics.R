fb3efa758b90dd3948805dd0a902de1454ce432c
Marly Gotti <marlygotti@gmail.com>
2022-01-31 14:14:38 -0500

    Use metrixBox on community usage metrics.

--- Server/communityusage_metrics.R
+++ Server/communityusage_metrics.R
@@ -1,6 +1,72 @@
 # IntroJS.
 introJSServer(id = "cum_introJS", text = cum_steps)
 
+#' Creates three metricBoxes: the time since first release, the time since
+#' latest release, and the number of downloads since last year.
+observeEvent(community_usage_metrics(), {
+  
+  # Get the first package release.
+  first_version <- community_usage_metrics() |>
+    filter(year == min(year) & month == min(month)) |>
+    slice_head(n = 1)
+  
+  # Get difference between today and first release in years.
+  time_diff_first_version <- year(Sys.Date()) - first_version$year
+  first_version_label <- 'Years'
+  if(time_diff_first_version == 0){
+    # Get difference in months.
+    time_diff_first_version <- month(Sys.Date()) - first_version$month
+    first_version_label <- 'Months'
+  }
+  if(time_diff_first_version == 1)
+    first_version_label <- str_remove(
+      string = first_version_label, pattern = 's$')
+  
+  metricBoxServer(id = 'time_since_first_version',
+                  title = 'First Version Release',
+                  desc = 'Time passed since first version release',
+                  value = glue('{time_diff_first_version}
+                               {first_version_label} Ago'),
+                  succ_icon = 'black-tie',
+                  icon_class = "text-info")
+  
+  # Get the last package release.
+  last_version <- community_usage_metrics() |>
+    filter(year == max(year) & month == max(month)) |>
+    slice_head(n = 1)
+  
+  # Get difference between today and latest release.
+  time_diff_latest_version <- year(Sys.Date()) - last_version$year
+  latest_version_label <- 'Years'
+  if(time_diff_latest_version == 0) {
+    # Get difference in months.
+    time_diff_latest_version <- month(Sys.Date()) - last_version$month
+    latest_version_label <- 'Months'
+  }
+  if(time_diff_latest_version == 1)
+    latest_version_label <- str_remove(
+      string = latest_version_label, pattern = 's$')
+  
+  metricBoxServer(id = 'time_since_latest_version',
+                  title = 'Lastest Version Release',
+                  desc = 'Time passed since latest version release',
+                  value = glue('{time_diff_latest_version}
+                               {latest_version_label} Ago'),
+                  succ_icon = 'meteor',
+                  icon_class = "text-info")
+  
+  downloads_last_year <- community_usage_metrics() |>
+    filter(year == year(Sys.Date()) - 1) |>
+    distinct(year, month, downloads)
+  
+  metricBoxServer(id = 'downloads_last_year',
+                  title = 'Package Downloads',
+                  desc = 'Number of downloads since last year',
+                  value = sum(downloads_last_year$downloads),
+                  succ_icon = 'box-open',
+                  icon_class = "text-info")
+})
+
 # Call module to create comments and save the output.
 cum_comment_added <- addCommentServer(id = "add_comment_for_cum",
                                       metric_abrv = 'cum',
@@ -15,96 +81,9 @@ viewCommentsServer(id = "view_cum_comments",
                    comment_type = 'cum')
 
 
-
-
-# 1. Render Output info box to show the content for time since first release.
-output$time_since_first_release <- renderInfoBox({
-  req(values$time_since_first_release_info)
-  infoBox(
-    title = "Package Maturity",
-    values$time_since_first_release_info,
-    subtitle = ifelse(values$time_since_first_release_info != "NA",
-                      "Months since first release.",
-                      "Metric is not applicable for this source of package."),
-    icon = shiny::icon("calendar"),
-    width = 3,
-    fill = TRUE
-  )
-})  # End of the time since first release render Output.
-
-# 2. Render Output info box to show the content for time since version release.
-
-output$time_since_version_release <- renderInfoBox({
-  req(values$time_since_version_release_info)
-  infoBox(
-    title = "Version Maturity",
-    values$time_since_version_release_info,
-    subtitle = ifelse(values$time_since_version_release_info != "NA", 
-                      "Months since version release.",
-                      "Metric is not applicable for this source of package."),
-    icon = shiny::icon("calendar"),
-    width = 3,
-    fill = TRUE
-  )
-  
-})  # End of the time since version release render Output.
-
-
-
-# 2.5 Render Output info box to show the number of downloads last year
-
-output$dwnlds_last_yr <- renderInfoBox({
-  req(values$no_of_downloads_last_year_info)
-  infoBox(
-    title = "Download Count",
-    formatC(values$no_of_downloads_last_year_info, format="f", big.mark=",", digits=0),
-    subtitle = ifelse(values$no_of_downloads_last_year_info != "NA",
-                      "Downloads in Last Year",
-                      "Metric is not applicable for this source of package."),
-    icon = shiny::icon("signal"),
-    width = 3,
-    fill = TRUE
-  )
-  
-})  # End 
-
-
 # 3. Render Output to show the plot for number of downloads on the application.
-output$no_of_downloads <- 
-  plotly::renderPlotly({
-    num_dwnlds_plot(data = values$riskmetrics_cum,
-                    input_select_pack = selected_pkg$name())
-  })
-
-
-
-
-
-
-observeEvent(input$submit_cum_comment, {
-  if (trimws(input$cum_comment) != "") {
-    db_ins(
-      paste0(
-        "INSERT INTO Comments values('",
-        selected_pkg$name(),
-        "',",
-        "'",
-        values$name,
-        "'," ,
-        "'",
-        values$role,
-        "',",
-        "'",
-        input$cum_comment,
-        "',",
-        "'cum',",
-        "'",
-        TimeStamp(),
-        "'"  ,
-        ")" 
-      )
-    )
-    updateTextAreaInput(session, "cum_comment", value = "")
-    # After comment added to Comments table, update db dash
-  }
-}) 
+# output$no_of_downloads <- 
+#   plotly::renderPlotly({
+#     num_dwnlds_plot(data = values$riskmetrics_cum,
+#                     input_select_pack = selected_pkg$name())
+#   })
--- UI/communityusage_metrics.R
+++ UI/communityusage_metrics.R
@@ -11,24 +11,16 @@ output$community_usage_metrics <- renderUI({
       introJSUI("cum_introJS"),
       br(), br(),
       fluidRow(
-        div(style = "height:25px;"),
-        class = "c_u_m_row_main",
         fluidRow(
-          id = "cum_infoboxes",
-          class = "c_u_m_row_1",
-          infoBoxOutput("time_since_first_release", width = 4),  # Info box to show the time since First release.
-          infoBoxOutput("time_since_version_release", width = 4),  # Info box to show the time since version release.
-          infoBoxOutput("dwnlds_last_yr", width = 4)  # Info box to show the total # of Downloads in the last year.
+          column(width = 4, metricBoxUI('time_since_first_version')),
+          column(width = 4, metricBoxUI("time_since_latest_version")),
+          column(width = 4, metricBoxUI('downloads_last_year'))
         ),
         fluidRow(
-          id = "cum_plot",
-          class = "c_u_m_row_graph",
-          column(width = 1, ),
-          column(width = 10,
-                 class = "w-90",
-                 plotly::plotlyOutput("no_of_downloads")
-          ),
-          column(width = 1, )
+          # column(width = 10,
+          #        class = "w-90",
+          #        plotly::plotlyOutput("no_of_downloads")
+          # )
         ),
         
         addCommentUI(id = "add_comment_for_cum"),
