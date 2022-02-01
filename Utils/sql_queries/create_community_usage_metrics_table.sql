702a175ed5891d6488729ff449d05f571433dbb1
Marly Gotti <marlygotti@gmail.com>
2022-02-01 11:01:46 -0500

    Update id of community_usage_metrics.

--- Utils/sql_queries/create_community_usage_metrics_table.sql
+++ Utils/sql_queries/create_community_usage_metrics_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS community_usage_metrics (
-  cum_id 					           CHAR NOT NULL, 
+  id 					               CHAR NOT NULL, 
   month					             INT, /* Format: 01 */
   year					             INT, /* Format: 2022 */
   downloads 			           INT,
   version    				         CHAR,
-  FOREIGN KEY(cum_id) REFERENCES package(name)
+  FOREIGN KEY(id) REFERENCES package(name)
 );
