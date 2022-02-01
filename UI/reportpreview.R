e7059009d02a4a38fb8d75d6b948e2ba41ba0516
Marly Gotti <marlygotti@gmail.com>
2022-02-01 11:00:48 -0500

    Overhaul report preview.

--- UI/reportpreview.R
+++ UI/reportpreview.R
@@ -2,50 +2,45 @@
 output$report_preview <- renderUI({
   req(selected_pkg$name())
   
-  if(selected_pkg$name() == "-")
-    showSelectPackageMessage()
-  
-  else {
-    removeUI(selector = "#Upload")
+  fluidPage(
     
-    shiny::tagList(
-      br(),
-      introJSUI("report_introJS"),
-      br(), br(),
-      fluidRow(
+    if(selected_pkg$name() == "-")
+      showSelectPackageMessage()
+    
+    else {
+      tagList(
+        br(),
+        introJSUI("report_introJS"),
+        h4("Report Preview", style = "text-align: center;"),
+        br(), br(),
+        
+        fluidRow(column(width = 12, selectInput("report_format", "Select Format", c("html", "docx")))),
+        fluidRow(column(width = 12, downloadButton("download_report_btn", "Download Report"))),
+        
         fluidRow(
-          id = "dwnld_rp",
-          tags$div(
-            selectInput("report_format", "Select Format", c("html", "docx")),  # Select input to select the format for report.
-          ),
-          tags$div(
-            downloadButton("download_report_btn", "Download Report", class = "download_report_btn_class btn-secondary"),  # Download button to export the report.
-          ),
-        ),
-        fluidRow(
-          id = "rep_prev",
           column(
             width = 12,
-            htmlOutput("gen_info"),  # Display General Information of the selected Package.
-            htmlOutput("decision_display"),  # Display the status of the Decision of a selected Package.
-            h3(tags$b(paste0('Overall Comments(',nrow(values$comment_o2),'):'))),
-            fluidRow(
-              column(
-                width = 12,
-                align = "left",
-                htmlOutput("overall_comments")  # Display the overall comment for selected Package. 
-              )
-            ),
-            fluidRow(
-              h3("Maintenance Metrics"),
-              metricGridUI("report_mm_metricGrid"),
-              viewCommentsUI("view_mm_comments_for_report")
-            ),
-            source(file.path("UI", "cum_report.R"), local = TRUE)$value,
-            # source(file.path("UI", "tm_report.R"), local = TRUE)$value
-          )
+            uiOutput("pkg_overview"),
+            uiOutput("decision_display"))
+        ),
+        
+        fluidRow(
+          column(width = 12,
+                 h5(glue('Overall Comments ({nrow(values$comment_o2)}):')),
+                 viewCommentsUI("view_overall_comments_for_report"))
+        ),
+        fluidRow(
+          h5("Maintenance Metrics"),
+          metricGridUI("report_mm_metricGrid"),
+          viewCommentsUI("view_mm_comments_for_report")
+        ),
+        
+        fluidRow(
+          h5("Community Usage Metrics"),
+          metricGridUI("report_mm_metricGrid"),
+          viewCommentsUI("view_cum_comments_for_report")
         )
       )
-    )
-  }
+    }
+  )
 })
