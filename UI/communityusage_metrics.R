c44b67ab9eb5a4231a3247d9dcfee3014834512f
Marly Gotti <marlygotti@gmail.com>
2022-02-01 10:56:52 -0500

    Replace community usage metrics.

--- UI/communityusage_metrics.R
+++ UI/communityusage_metrics.R
@@ -2,30 +2,32 @@
 output$community_usage_metrics <- renderUI({
   req(selected_pkg$name())
   
-  if(selected_pkg$name() == "-")
-    showSelectPackageMessage()
-  
-  else {
-    shiny::tagList(
-      br(),
-      introJSUI("cum_introJS"),
-      br(), br(),
-      fluidRow(
+  fluidPage(
+    
+    if(selected_pkg$name() == "-")
+      showSelectPackageMessage()
+    
+    else {
+      tagList(
+        br(),
+        introJSUI("cum_introJS"),
+        h4("Community Usage Metrics", style = "text-align: center;"),
+        br(), br(),
+        # TODO: change this for a grid.
         fluidRow(
           column(width = 4, metricBoxUI('time_since_first_version')),
           column(width = 4, metricBoxUI("time_since_latest_version")),
           column(width = 4, metricBoxUI('downloads_last_year'))
         ),
+        br(), br(),
         fluidRow(
-          # column(width = 10,
-          #        class = "w-90",
-          #        plotly::plotlyOutput("no_of_downloads")
-          # )
-        ),
-        
-        addCommentUI(id = "add_comment_for_cum"),
-        viewCommentsUI(id = "view_cum_comments")
+          column(width = 12, style = 'padding-left: 20px; padding-right: 20px;',
+                 plotlyOutput("downloads_plot"))),
+        br(), br(),
+        fluidRow(
+          addCommentUI(id = "add_comment_for_cum"),
+          viewCommentsUI(id = "view_cum_comments"))
       )
-    )
-  } 
+    }
+  )
 })
