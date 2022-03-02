9b8608f1af35198a1eb1ca9433b05cb8a4700b60
Robert Krajcik <robert.krajcik@biogen.com>
2022-03-02 13:28:53 -0500

    remove unnecessary validate() inside update package version step

--- R/sidebar.R
+++ R/sidebar.R
@@ -99,12 +99,9 @@ sidebarServer <- function(id, uploaded_pkgs, user) {
     # Update package version.
     observeEvent(input$select_pkg, {
       req(input$select_pkg)
-      
-      if(input$select_pkg == "-")
-        validate("Please select a package")
-      
+
       version <- ifelse(input$select_pkg == "-", "-", selected_pkg()$version)
-      
+
       updateSelectizeInput(
         session,
         "select_ver",
