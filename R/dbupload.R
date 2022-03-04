777828128b603e40c2db9f2e65fcc73a853ebbdf
Aaron Clark <aaron.clark@biogen.com>
2022-03-04 11:07:09 -0500

    grab existing code from another function that gets latest package info, fix some bugs mentioned in #171

--- R/dbupload.R
+++ R/dbupload.R
@@ -1,44 +1,53 @@
-# Get the package general information from CRAN/local and upload it to db.
+
+# Get the package general information from CRAN/local
+get_latest_pkg_info <- function(pkg_name) {
+  webpage <- read_html(glue(
+    'https://cran.r-project.org/web/packages/{pkg_name}'))
+  
+  #' Regex that finds entry: '\n ', "'", and '"' (the `|` mean 'or' and the 
+  #' `\`` is to scape the double quotes).
+  pattern <- '\n |\'|\"|\\"'
+  
+  # Save div with class container to get the title and description.
+  div_container <- webpage %>% html_nodes("div.container")
+  
+  # Read package title and clean it.
+  title <- div_container %>% 
+    html_nodes("h2") %>% 
+    html_text() %>%
+    str_remove_all(pattern = pattern)
+  
+  # Read package description and clean it.
+  description <- div_container %>% 
+    html_nodes("h2 + p") %>% 
+    html_text() %>%
+    str_remove_all(pattern = pattern)
+  
+  # Get the table displaying version, authors, etc.
+  table_info <- (webpage %>% html_table())[[1]] %>%
+    mutate(X1 = str_remove_all(string = X1, pattern = ':')) %>%
+    mutate(X2 = str_remove_all(string = X2, pattern = pattern)) %>%
+    pivot_wider(names_from = X1, values_from = X2) %>%
+    select(Version, Maintainer, Author, License, Published) %>%
+    mutate(Title = title, Description = description)
+  
+  return(table_info)
+}
+
+
+# Call function to get and upload info from CRAN/local to db.
 # TODO: get and upload should be distinct functions.
 insert_pkg_info_to_db <- function(pkg_name) {
   tryCatch(
     expr = {
-      webpage <- read_html(glue(
-          'https://cran.r-project.org/web/packages/{pkg_name}'))
+      # get latest high-level package info
+      pkg_info <- get_latest_pkg_info(pkg_name)
       
-      #' Regex that finds entry: '\n ', "'", and '"' (the `|` mean 'or' and the 
-      #' `\`` is to scape the double quotes).
-      pattern <- '\n |\'|\"|\\"'
-      
-      # Save div with class container to get the title and description.
-      div_container <- webpage %>% html_nodes("div.container")
-      
-      # Read package title and clean it.
-      title <- div_container %>% 
-        html_nodes("h2") %>% 
-        html_text() %>%
-        str_remove_all(pattern = pattern)
-      
-      # Read package description and clean it.
-      description <- div_container %>% 
-        html_nodes("h2 + p") %>% 
-        html_text() %>%
-        str_remove_all(pattern = pattern)
-      
-      # Get the table displaying version, authors, etc.
-      #' TODO: this variable is redundant for now. Split the entire function into
-      #' two: get the data in one function and upload in another.
-      table_info <- (webpage %>% html_table())[[1]] %>%
-        mutate(X1 = str_remove_all(string = X1, pattern = ':')) %>%
-        mutate(X2 = str_remove_all(string = X2, pattern = pattern)) %>%
-        pivot_wider(names_from = X1, values_from = X2) %>%
-        select(Version, Maintainer, Author, License, Published) %>%
-        mutate(Title = title, Description = description)
-      
-      upload_package_to_db(pkg_name, table_info$Version, table_info$Title,
-                           table_info$Description, table_info$Author,
-                           table_info$Maintainer, table_info$License,
-                           table_info$Published)
+      # store it in the database
+      upload_package_to_db(pkg_name, pkg_info$Version, pkg_info$Title,
+                           pkg_info$Description, pkg_info$Author,
+                           pkg_info$Maintainer, pkg_info$License,
+                           pkg_info$Published)
       
     },
     error = function(e) {
@@ -149,6 +158,7 @@ insert_maintenance_metrics_to_db <- function(pkg_name){
 
 
 # Get community usage metrics info and upload into DB.
+# pkg_name = "dplyr"
 insert_community_metrics_to_db <- function(pkg_name) {
   
   pkgs_cum_metrics <- tibble()
@@ -164,10 +174,15 @@ insert_community_metrics_to_db <- function(pkg_name) {
         html_table() %>%
         select(-c("", "Description", 'Size')) %>%
         filter(`Last modified` != "") %>%
-        mutate(date = as.Date(`Last modified`), .keep = 'unused') %>%
         mutate(version = str_remove_all(
           string = Name, pattern = glue('{pkg_name}_|.tar.gz')),
           .keep = 'unused') %>%
+        # get latest high-level package info
+        union(
+          get_latest_pkg_info(pkg_name) %>%
+            select(`Last modified` = Published, version = Version)
+        ) %>%
+        mutate(date = as.Date(`Last modified`), .keep = 'unused') %>%
         mutate(month = month(date)) %>%
         mutate(year = year(date))
 
@@ -175,20 +190,23 @@ insert_community_metrics_to_db <- function(pkg_name) {
       first_release_date <- versions_with_dates %>%
         pull(date) %>%
         min()
-      
+
       # Get the number of downloads by month, year.
-      pkgs_cum_metrics <- cranlogs::cran_downloads(pkg_name,
-                                                   from = first_release_date,
-                                                   to = Sys.Date()) %>%
-        filter(month(date) != month(Sys.Date())) %>%
-        mutate(month = month(date)) %>%
-        mutate(year = year(date)) %>%
+      pkgs_cum_metrics <- 
+        cranlogs::cran_downloads(
+          pkg_name,
+          from = first_release_date,
+          to = Sys.Date()) %>%
+        mutate(month = month(date),
+               year = year(date)) %>%
+        filter(!(month == month(Sys.Date()) &
+                 year == year(Sys.Date()))) %>%
         group_by(month, year) %>%
         summarise(downloads = sum(count)) %>%
         ungroup() %>%
         left_join(versions_with_dates, by = c('month', 'year')) %>%
+        arrange(year, month) %>%
         select(-date)
-      
     },
     error = function(e) {
       loggit("ERROR", paste("Error extracting cum metric info of the package:",
