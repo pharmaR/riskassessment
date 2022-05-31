7fbdb778159ac7e4874b99b8a8e17ae05b888e73
Marly Gotti <marlygotti@gmail.com>
2022-05-31 13:50:11 -0400

    Remove commented code.

--- global.R
+++ global.R
@@ -39,59 +39,21 @@ package.check <- lapply(
   packages, FUN = function(x) { library(x, character.only = TRUE) }
 )
 
-app_version <- 'beta'
-
-# Install and load required packages.
-# package.check <- lapply(
-#   packages,
-#   FUN = function(x) {
-#     if (!require(x, character.only = TRUE)) {
-#       install.packages(x, dependencies = TRUE)
-#       library(x, character.only = TRUE)
-#     }
-#   }
-# )
-
-# Install a specific version of riskmetric from GitHub and load it.
-# if(!require(riskmetric)){
-#   devtools::install_github("pharmaR/riskmetric", ref = "release-v0.1.1")
-#   library(riskmetric)
-# }
-
-
 # Note: If deploying the app to shinyapps.io, then the code to directly install
 # missing packages will need to be removed as the app will fail to deploy.
-# Instead comment the code that install packages and attach them directly by
-# uncommenting the lines below.
 
-# library(shiny)
-# library(shinyhelper)
-# library(shinyjs)
-# library(shinydashboard)
-# library(shinyWidgets)
-# library(data.table)
-# library(DT)
-# library(readr)
-# library(lubridate)
-# library(RSQLite)
-# library(DBI)
-# library(rvest)
-# library(xml2)
-# library(httr)
-# library(desc)
-# library(dplyr)
-# library(tools)
-# library(stringr)
-# library(tidyverse)
-# library(loggit)
-# library(shinycssloaders)
-# library(rAmCharts)
-# library(devtools)
-# library(plotly)
-# library(riskmetric) # devtools::install_github("pharmaR/riskmetric")
-# library(formattable)
-# library(rintrojs)
+# Install and load required packages.
+package.check <- lapply(
+  packages,
+  FUN = function(x) {
+    if (!require(x, character.only = TRUE)) {
+      install.packages(x, dependencies = TRUE)
+      library(x, character.only = TRUE)
+    }
+  }
+)
 
+app_version <- 'beta'
 passphrase <- 'somepassphrase'
 
 #' Displays a helper message. By default, it informs the user that he should
