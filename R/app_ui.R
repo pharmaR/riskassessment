0c6ba7263b2339ed5ec39787fa60cb990a07affe
Robert Krajcik <robert.krajcik@biogen.com>
2022-11-17 08:41:58 -0500

    add import(shiny, except =) to app_ui.R

--- R/app_ui.R
+++ R/app_ui.R
@@ -2,7 +2,7 @@
 #'
 #' @param request Internal parameter for `{shiny}`.
 #'     DO NOT REMOVE.
-#' @import shiny
+#' @rawNamespace import(shiny, except = c(dataTableOutput, renderDataTable))
 #' @importFrom shinymanager secure_app
 #' 
 #' @noRd
