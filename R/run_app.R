90a175c7b510d1b81b409bee7aa079741bbaf738
Jeff Thompson <jeff.thompson1@biogen.com>
2022-11-21 15:37:23 -0500

    Make sure code does not run too early

--- R/run_app.R
+++ R/run_app.R
@@ -42,12 +42,11 @@ run_app <- function(
       ui = add_tags(shinymanager::secure_app(app_ui,
         tags_top = tags$div(
             tags$head(tags$style(HTML(readLines(system.file("app", "www", "css", "login_screen.css", package = "riskassessment"))))),
-            tags$head(if (!get_golem_config("app_prod") && !is.null(golem::get_golem_options("login_creds"))) {tags$script(HTML(glue::glue("function pageLoaded() {{
+            tags$head(if (!get_golem_config("app_prod") && !is.null(golem::get_golem_options("login_creds"))) {tags$script(HTML(glue::glue("$(document).on('shiny:connected', function () {{
     Shiny.setInputValue('auth-user_id', '{golem::get_golem_options('login_creds')$user_id}');
     Shiny.setInputValue('auth-user_pwd', '{golem::get_golem_options('login_creds')$user_pwd}');
-    document.getElementById('auth-go_auth').click();
-  }}
-  window.onload = pageLoaded;")))}),
+    $('#auth-go_auth').trigger('click');
+  }});")))}),
             id = "login_screen",
             tags$h2("Risk Assessment Application", style = "align:center"),
             tags$h3(glue::glue('**Version {app_ver}**'),
