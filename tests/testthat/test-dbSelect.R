c3a27b766a86104cfa3189039b2385729608fdc8
Robert Krajcik <robert.krajcik@biogen.com>
2022-12-08 08:38:44 -0500

    update test-dbSelect

--- tests/testthat/test-dbSelect.R
+++ tests/testthat/test-dbSelect.R
@@ -2,17 +2,19 @@ test_that("database has been defined and dbSelect function works", {
   db_name = golem::get_golem_options('assessment_db_name')
   if(is.null(db_name)) db_name <- "database.sqlite"
   
+  setwd(devtools::wd())
+  
   # 1. file exists?
-  testthat::expect_true(file.exists(db_name))
+  testthat::expect_true(file.exists(file.path(devtools::wd(), db_name)))
   
   # 2. valid db? test this is a valid sqlite database
-  testthat::expect_equal(readLines(file.path("../..",db_name), n =1, warn = FALSE), "SQLite format 3")  
+  testthat::expect_equal(readLines(file.path(devtools::wd(), db_name), n =1, warn = FALSE), "SQLite format 3")  
   
   # 3. can we connect?
   testthat::expect_true(DBI::dbCanConnect(RSQLite::SQLite(), db_name))
 
   query <- "SELECT [name] FROM sqlite_master WHERE type='table' order by [rootpage]"
-  tbls <- dbSelect(query, file.path("../..",db_name))
+  tbls <- dbSelect(query, file.path(devtools::wd(), db_name))
   
   # 4. test s3 class is data.frame
   testthat::expect_s3_class(tbls, "data.frame")
@@ -22,7 +24,7 @@ test_that("database has been defined and dbSelect function works", {
   testthat::expect_equal(tbls |> dplyr::pull(), tbl_names)
   
   query <- "select * FROM [metric] WHERE [class] = 'maintenance' order by [id]"
-  metric <- dbSelect(query, file.path("../..",db_name))
+  metric <- dbSelect(query, file.path(devtools::wd(), db_name))
   
   # 6. test that we have at least 10 maintenance metrics
   testthat::expect_gte(nrow(metric), 10)
