61b44701b3aa8aee15f89927fde8fc0ea3fca8e8
imran3004 <munshiimranhossain@gmail.com>
2022-12-09 21:40:38 +0530

    Add argument names to test cases.

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,14 +1,12 @@
 
 ## test cases for functions in utils.R
-
-
 testthat::test_that(desc = "Check output of get_date_span()", 
                     code = {
-                      testthat::expect_equal(get_date_span(start = "2021-07-03", end = "2022-12-05")$value, 1)
-                      testthat::expect_equal(get_date_span(start = "2021-07-03", end = "2022-12-05")$label, "Year")
-                      testthat::expect_equal(get_date_span(start = "2020-02-26", end = "2020-05-04")$value, 2)
-                      testthat::expect_equal(get_date_span(start = "2020-02-26", end = "2020-05-04")$label, "Months")
-                      testthat::expect_equal(get_date_span(start = "2020-03-16", end = "2020-03-21")$value, 0)
-                      testthat::expect_equal(get_date_span(start = "2020-03-16", end = "2020-03-21")$label, "Months")
+                      testthat::expect_equal(object = get_date_span(start = "2021-07-03", end = "2022-12-05")$value, expected = 1)
+                      testthat::expect_equal(object = get_date_span(start = "2021-07-03", end = "2022-12-05")$label, expected = "Year")
+                      testthat::expect_equal(object = get_date_span(start = "2020-02-26", end = "2020-05-04")$value, expected = 2)
+                      testthat::expect_equal(object = get_date_span(start = "2020-02-26", end = "2020-05-04")$label, expected = "Months")
+                      testthat::expect_equal(object = get_date_span(start = "2020-03-16", end = "2020-03-21")$value, expected = 0)
+                      testthat::expect_equal(object = get_date_span(start = "2020-03-16", end = "2020-03-21")$label, expected = "Months")
                     })
 
