f827c8ea4c76bef6dfb9b3059041ec25ac6eb5ff
imran3004 <munshiimranhossain@gmail.com>
2022-12-10 22:21:01 +0530

    Added a test case to check for error message.

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -8,5 +8,7 @@ testthat::test_that(desc = "Check output of get_date_span()",
                       testthat::expect_equal(object = get_date_span(start = "2020-02-26", end = "2020-05-04")$label, expected = "Months")
                       testthat::expect_equal(object = get_date_span(start = "2020-03-16", end = "2020-03-21")$value, expected = 0)
                       testthat::expect_equal(object = get_date_span(start = "2020-03-16", end = "2020-03-21")$label, expected = "Months")
+                      
+                      ## check error message when function is called without any argument
+                      testthat::expect_error(object = get_date_span(), regexp = 'argument "start" is missing, with no default')
                     })
-
