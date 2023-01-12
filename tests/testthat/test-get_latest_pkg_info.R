3da3f67a8ea94e8b1b899b921acde8eeea8d9c61
Andersen <lars.andersen@boehringer-ingelheim.com>
2023-01-12 09:37:31 +0100

    changes requested in PR

--- tests/testthat/test-get_latest_pkg_info.R
+++ tests/testthat/test-get_latest_pkg_info.R
@@ -1,22 +1,9 @@
-describe("utils.R", {
-  describe("get_latest_pkg_info", {
-    it("should return a list", {
+test_that("utils.R", {
       test <- get_latest_pkg_info("rpact")
       
       expect_type(test, "list")
-    })
-    it("should have the variables: Version, Maintainer,
-       Author, License, Published, Title, Description in it",  {
-      test <- get_latest_pkg_info("rpact")
-      
       expect_equal(colnames(test), c("Version", "Maintainer", "Author",
                                      "License", "Published", "Title",
                                      "Description"))
-    })
-    it("should not return an empty list", {
-      test <- get_latest_pkg_info("rpact")
-      
       expect_true(length(test) > 0)
-    })
-  })
 })
