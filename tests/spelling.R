ccf3bdba3693b0d437218b1451ec6e9b36d616fd
AARON-CLARK <clark.aaronchris@gmail.com>
2023-01-25 12:42:36 -0500

    fixed spelling errors and set spelling unit test to error if a new mispelled word is found.

--- tests/spelling.R
+++ tests/spelling.R
@@ -1,3 +1,8 @@
 if(requireNamespace('spelling', quietly = TRUE))
-  spelling::spell_check_test(vignettes = TRUE, error = FALSE,
+  spelling::spell_check_test(vignettes = TRUE, error = TRUE,
                              skip_on_cran = TRUE)
+# To check for spelling mistakes:
+# spelling::spell_check_package()
+
+# To add remainging (un-correctable) words to the wordlist:
+# spelling::update_wordlist()
