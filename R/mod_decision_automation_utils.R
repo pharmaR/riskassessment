e5138e5c23423bb48bbc5a4ab436059aedad4531
Jeff Thompson <jeff.thompson1@biogen.com>
2023-02-06 16:48:59 -0500

    Fix comment update for auto classification

--- R/mod_decision_automation_utils.R
+++ R/mod_decision_automation_utils.R
@@ -8,14 +8,11 @@ assign_decisions <- function(decision_list, package) {
     dbUpdate(glue::glue("UPDATE package SET decision = '{decision}' WHERE name = '{package}'"))
     loggit::loggit("INFO",
                    glue::glue("decision for the package {package} was assigned {decision} by decision automation rules"))
-    comment <- glue::glue("Risk was assigned '{decision}' by decision rules because the risk score was between {decision_list[[decision]][1]} and {decision_list[[decision]][2]}")
-    dbUpdate(
-      glue::glue(
-        "UPDATE comments
-          SET comment = '{comment}', added_on = '{getTimeStamp()}'
-          WHERE id = '{package}' AND
-          comment_type = 'o'"
-      ))
+    comment <- glue::glue("Risk was assigned ''{decision}'' by decision rules because the risk score was between {decision_list[[decision]][1]} and {decision_list[[decision]][2]}")
+    dbUpdate(glue::glue(
+      "INSERT INTO comments
+          VALUES ('{package}', 'auto_assign', 'admin',
+          '{comment}', 'o', '{getTimeStamp()}')"))
   }
   
   return(decision)
