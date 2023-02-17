8eb3b1f92a0fd98b57b6331371a77faf3977d7f9
Jeff Thompson <jeff.thompson1@biogen.com>
2023-02-17 16:12:51 -0500

    Add nonadmin test for introJS

--- tests/testthat/test-introJS.R
+++ tests/testthat/test-introJS.R
@@ -117,3 +117,70 @@ test_that("The introJS module works as expected for admins", {
   
   app$stop()
 })
+
+test_that("The introJS module works as expected for nonadmins", {
+  app_db_loc <- test_path("test-apps", "nonadmin-app", "database.sqlite")
+  if (file.exists(app_db_loc)) {
+    file.remove(app_db_loc)
+  }
+  
+  # copy in already instantiated database to avoid need to rebuild
+  # this is a database that has been built via inst/testdata/upload_format.csv
+  test_db_loc <- system.file("testdata", "skeleton.sqlite", package = "riskassessment")
+  file.copy(
+    test_db_loc,
+    app_db_loc
+  )
+  
+  getBoundingClientRect <- function(appDriver, el) {
+    appDriver$get_js(glue::glue('const rect = $("{el}")[0].getBoundingClientRect(); [rect.left, rect.top, rect.bottom, rect.right]')) %>% purrr::possibly(purrr::set_names, otherwise = .)(c("left", "top", "bottom", "right"))
+  }
+  
+  # set up new app driver object
+  app <- shinytest2::AppDriver$new(app_dir = test_path("test-apps", "nonadmin-app"))
+  app$set_window_size(width = 1619, height = 1057)
+  app$wait_for_idle()
+  
+  expect_equal(app$get_value(input = "tabs"), "Upload Package")
+  
+  # note upload_pkgs 
+  upload_pkgs <- bind_rows(list(upload_pkg, apptab_steps, sidebar_steps))
+  
+  app$click("upload_package-introJS-help")
+  app$wait_for_idle()
+  
+  # Verify that all elements exist and are visible
+  el_pos <- purrr::map(upload_pkgs$element, getBoundingClientRect, appDriver = app)
+  introjs_bullets <- app$get_js("$('.introjs-bullets ul li').length")
+  expect(length(el_pos) == introjs_bullets, "One or more Upload Package introJS elements are missing.")
+  expect(all(purrr::map_lgl(el_pos, ~ any(.x > 0))), "One or more elements are not visible")
+  steps <- app$get_value(export = "upload_package-introJS-steps")
+  expect_equal(upload_pkgs, steps)
+  
+  app$click(selector = ".introjs-skipbutton")
+  
+  app$run_js("Shiny.setInputValue('upload_package-load_cran', 'load')")
+  app$wait_for_idle()
+  app$set_inputs(`upload_package-pkg_lst` = "tidyr")
+  app$click("upload_package-add_pkgs", wait_ = FALSE)
+  app$wait_for_value(
+    output = "upload_package-upload_pkgs_table",
+    ignore = list(NULL),
+    timeout = 30 * 1000
+  )
+  
+  app$click("upload_package-introJS-help")
+  app$wait_for_idle()
+  
+  upload_pkg_complete <- dplyr::bind_rows(list(upload_pkg, upload_pkg_comp, apptab_steps, sidebar_steps))
+  
+  # Verify that all elements exist and are visible
+  el_pos <- purrr::map(upload_pkg_complete$element, getBoundingClientRect, appDriver = app)
+  introjs_bullets <- app$get_js("$('.introjs-bullets ul li').length")
+  expect(length(el_pos) == introjs_bullets, "One or more Upload Package introJS elements are missing.")
+  expect(all(purrr::map_lgl(el_pos, ~ any(.x > 0))), "One or more elements are not visible")
+  steps <- app$get_value(export = "upload_package-introJS-steps")
+  expect_equal(upload_pkg_complete, steps)
+  
+  app$stop()
+})
