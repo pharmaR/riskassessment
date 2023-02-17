0113ea3a97fdf0648f7cae659233e29e07429fd5
Jeff Thompson <jeff.thompson1@biogen.com>
2023-02-17 15:35:05 -0500

    Make test less dependent on UI responsiveness and more maintainable

--- tests/testthat/test-introJS.R
+++ tests/testthat/test-introJS.R
@@ -1,4 +1,4 @@
-test_that("The introJS module works as expected", {
+test_that("The introJS module works as expected for admins", {
   app_db_loc <- test_path("test-apps", "database.sqlite")
   if (file.exists(app_db_loc)) {
     file.remove(app_db_loc)
@@ -12,47 +12,32 @@ test_that("The introJS module works as expected", {
     app_db_loc
   )
   
+  getBoundingClientRect <- function(appDriver, el) {
+    appDriver$get_js(glue::glue('const rect = $("{el}")[0].getBoundingClientRect(); [rect.left, rect.top, rect.bottom, rect.right]')) %>% purrr::possibly(purrr::set_names, otherwise = .)(c("left", "top", "bottom", "right"))
+  }
+  
   # set up new app driver object
   app <- shinytest2::AppDriver$new(app_dir = test_path("test-apps"))
+  app$set_window_size(width = 1619, height = 1057)
+  app$wait_for_idle()
   
   expect_equal(app$get_value(input = "tabs"), "Upload Package")
   
   # note upload_pkgs 
-  upload_pkgs <- bind_rows(list(upload_pkg, upload_adm, apptab_admn, apptab_steps))
+  upload_pkgs <- bind_rows(list(upload_pkg, upload_adm, apptab_admn, apptab_steps, sidebar_steps))
   
   app$click("upload_package-introJS-help")
+  app$wait_for_idle()
   
   # Verify that all elements exist and are visible
-  el_pos <-
-    purrr::map(upload_pkgs$element, ~ app$get_js(glue::glue('$("{.x}").position()'))[["top"]]) %>%
-    unlist()
-  expect(length(el_pos) == length(upload_pkgs$element), "One or more Upload Package introJS elements are missing.")
-  #expect(all(el_pos != 0), "Not all Upload Package introJS elements are visible.") #last 3 not visible.
-  
-  tt_txt <- app$get_text(".introjs-tooltiptext")
-  expect_equal(tt_txt, upload_pkgs$intro[1])
-  
-  app$click(selector = ".introjs-nextbutton")
-  app$wait_for_idle()
-  
-  tt_txt <- app$get_text(".introjs-tooltiptext")
-  expect_equal(tt_txt, upload_pkgs$intro[2])
-  
-  app$click(selector = glue::glue("[data-stepnumber='{nrow(upload_pkgs)+1}']"))
-  app$wait_for_idle()
-  
-  tt_txt <- app$get_text(".introjs-tooltiptext")
-  expect_equal(tt_txt, sidebar_steps$intro[1])
-  
-  app$click(selector = ".introjs-prevbutton")
-  app$wait_for_idle()
-  
-  tt_txt <- app$get_text(".introjs-tooltiptext")
-  expect_equal(tt_txt, upload_pkgs$intro[nrow(upload_pkgs)])
-  
-  app$click(selector = glue::glue("[data-stepnumber='{nrow(upload_pkgs)+nrow(sidebar_steps)}']"))
-  app$wait_for_idle()
-  app$click(selector = ".introjs-donebutton")
+  el_pos <- purrr::map(upload_pkgs$element, getBoundingClientRect, appDriver = app)
+  introjs_bullets <- app$get_js("$('.introjs-bullets ul li').length")
+  expect(length(el_pos) == introjs_bullets, "One or more Upload Package introJS elements are missing.")
+  expect(all(purrr::map_lgl(el_pos, ~ any(.x > 0))), "One or more elements are not visible")
+  steps <- app$get_value(export = "upload_package-introJS-steps")
+  expect_equal(upload_pkgs, steps)
+
+  app$click(selector = ".introjs-skipbutton")
   
   app$run_js("Shiny.setInputValue('upload_package-load_cran', 'load')")
   app$wait_for_idle()
@@ -64,75 +49,71 @@ test_that("The introJS module works as expected", {
     timeout = 30 * 1000
   )
   
-  upload_pkg_complete <- dplyr::bind_rows(list(upload_pkg, upload_adm, upload_pkg_comp, apptab_admn, apptab_steps))
-  
-  # Verify that all elements exist and are visible
-  el_pos <-
-    purrr::map(upload_pkg_complete$element, ~ app$get_js(glue::glue('$("{.x}").position()'))[["top"]]) %>%
-    unlist()
-  expect(length(el_pos) == length(upload_pkg_complete$element), "One or more Upload Complete introJS elements are missing.")
-  #expect(all(el_pos != 0), "Not all Upload Complete introJS elements are visible.")
-  
   app$click("upload_package-introJS-help")
-  app$click(selector = glue::glue("[data-stepnumber='{nrow(upload_pkg_complete)+1}']"))
   app$wait_for_idle()
   
-  # we should be at sidebar_steps #1 here
-  tt_txt <- app$get_text(".introjs-tooltiptext")
-  expect_equal(tt_txt, sidebar_steps$intro[1])
+  upload_pkg_complete <- dplyr::bind_rows(list(upload_pkg, upload_adm, upload_pkg_comp, apptab_admn, apptab_steps, sidebar_steps))
+  
+  # Verify that all elements exist and are visible
+  el_pos <- purrr::map(upload_pkg_complete$element, getBoundingClientRect, appDriver = app)
+  introjs_bullets <- app$get_js("$('.introjs-bullets ul li').length")
+  expect(length(el_pos) == introjs_bullets, "One or more Upload Package introJS elements are missing.")
+  expect(all(purrr::map_lgl(el_pos, ~ any(.x > 0))), "One or more elements are not visible")
+  steps <- app$get_value(export = "upload_package-introJS-steps")
+  expect_equal(upload_pkg_complete, steps)
   
   app$click(selector = ".introjs-skipbutton")
   app$set_inputs(tabs = "Maintenance Metrics")
   app$set_inputs(`sidebar-select_pkg` = "tidyr")
   app$wait_for_idle()
   
-  # Verify that all elements exist and are visible
-  el_pos <-
-    purrr::map(mm_steps$element, ~ app$get_js(glue::glue('$("{.x}").position()'))[["top"]]) %>%
-    unlist()
-  expect(length(el_pos) == length(mm_steps$element), "One or more Maintenance Metrics introJS elements are missing.")
-  expect(all(el_pos != 0), "Not all Maintenance Metrics introJS elements are visible.")
-  
   app$click("maintenanceMetrics-introJS-help")
   app$wait_for_idle()
   
-  tt_txt <- app$get_text(".introjs-tooltiptext")
-  expect_equal(tt_txt, mm_steps$intro[1])
+  maintenance_metrics <- dplyr::bind_rows(mm_steps, apptab_admn, apptab_steps, sidebar_steps)
+  
+  # Verify that all elements exist and are visible
+  el_pos <- purrr::map(maintenance_metrics$element, getBoundingClientRect, appDriver = app)
+  introjs_bullets <- app$get_js("$('.introjs-bullets ul li').length")
+  expect(length(el_pos) == introjs_bullets, "One or more Upload Package introJS elements are missing.")
+  expect(all(purrr::map_lgl(el_pos, ~ any(.x > 0))), "One or more elements are not visible")
+  steps <- app$get_value(export = "maintenanceMetrics-introJS-steps")
+  expect_equal(maintenance_metrics, steps)
   
   app$click(selector = ".introjs-skipbutton")
   
   app$set_inputs(tabs = "Community Usage Metrics")
   app$wait_for_idle()
   
-  # Verify that all elements exist and are visible
-  el_pos <-
-    purrr::map(cum_steps$element, ~ app$get_js(glue::glue('$("{.x}").position()'))[["top"]]) %>%
-    unlist()
-  expect(length(el_pos) == length(cum_steps$element), "One or more Community Usage Metrics introJS elements are missing.")
-  expect(all(el_pos != 0), "Not all Community Usage Metrics introJS elements are visible.")
-  
   app$click("communityMetrics-introJS-help")
   app$wait_for_idle()
   
-  tt_txt <- app$get_text(".introjs-tooltiptext")
-  expect_equal(tt_txt, cum_steps$intro[1])
+  community_metrics <- dplyr::bind_rows(cum_steps, apptab_admn, apptab_steps, sidebar_steps)
+  
+  # Verify that all elements exist and are visible
+  el_pos <- purrr::map(community_metrics$element, getBoundingClientRect, appDriver = app)
+  introjs_bullets <- app$get_js("$('.introjs-bullets ul li').length")
+  expect(length(el_pos) == introjs_bullets, "One or more Upload Package introJS elements are missing.")
+  expect(all(purrr::map_lgl(el_pos, ~ any(.x > 0))), "One or more elements are not visible")
+  steps <- app$get_value(export = "communityMetrics-introJS-steps")
+  expect_equal(community_metrics, steps)
   
   app$click(selector = ".introjs-skipbutton")
   app$set_inputs(tabs = "Report Preview")
   app$wait_for_idle()
   
-  # Verify that all elements exist and are visible
-  el_pos <-
-    purrr::map(rp_steps$element, ~ app$get_js(glue::glue('$("{.x}").position()'))[["top"]]) %>%
-    unlist()
-  expect(length(el_pos) == length(rp_steps$element), "One or more Report Preview introJS elements are missing.")
-  expect(all(el_pos != 0), "Not all Report Preview introJS elements are visible.")
-  
   app$click("reportPreview-introJS-help")
   app$wait_for_idle()
   
-  tt_txt <- app$get_text(".introjs-tooltiptext")
-  expect_equal(tt_txt, rp_steps$intro[1])
+  report_preview <- dplyr::bind_rows(rp_steps, apptab_admn, apptab_steps, sidebar_steps)
+  
+  # Verify that all elements exist and are visible
+  el_pos <- purrr::map(report_preview$element, getBoundingClientRect, appDriver = app)
+  introjs_bullets <- app$get_js("$('.introjs-bullets ul li').length")
+  expect(length(el_pos) == introjs_bullets, "One or more Upload Package introJS elements are missing.")
+  expect(all(purrr::map_lgl(el_pos, ~ any(.x > 0))), "One or more elements are not visible")
+  steps <- app$get_value(export = "reportPreview-introJS-steps")
+  expect_equal(report_preview, steps)
   
   app$stop()
 })
