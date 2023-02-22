fe147ec82dd2cd44df9a34af568b18276f0b507c
Aaron Clark <aaron.clark@biogen.com>
2023-02-22 14:03:19 -0500

    updated app.R

--- app.R
+++ app.R
@@ -5,4 +5,4 @@
 pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
 options( "golem.app.prod" = TRUE)
 riskassessment::run_app(login_note = HTML('<em>Note:</em> To log in for the first time, use the admin user:
-                          <u>admin</u> with password <u>QWERTY1</u>.')) # add parameters here (if any)
+                          <u>admin</u> with password <u>QWERTY1</u>.')) 
