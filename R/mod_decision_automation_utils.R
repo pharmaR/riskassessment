5f831601536ac489668f908995f34302e6484ee2
Robert Krajcik <robert.krajcik@biogen.com>
2023-03-15 13:00:31 -0500

    update mod_decision_automation_utls.R

--- R/mod_decision_automation_utils.R
+++ R/mod_decision_automation_utils.R
@@ -5,7 +5,9 @@ assign_decisions <- function(decision_list, package) {
   score <- get_pkg_info(package)$score
   decision <- paste0(names(decision_list)[purrr::map_lgl(decision_list, ~ .x[1] < score && score <= .x[2])], "")
   if (decision != "") {
-    dbUpdate(glue::glue("UPDATE package SET decision = '{decision}' WHERE name = '{package}'"))
+    dbUpdate(glue::glue("UPDATE package SET decision = '{decision}',
+                        decision_by = 'auto_assign', decision_date = '{Sys.Date()}'
+                        WHERE name = '{package}'"))
     loggit::loggit("INFO",
                    glue::glue("decision for the package {package} was assigned {decision} by decision automation rules"))
     comment <- glue::glue("Risk was assigned ''{decision}'' by decision rules because the risk score was between {decision_list[[decision]][1]} and {decision_list[[decision]][2]}")
