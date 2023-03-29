c9801871f4864fe66ab7eef0a85221150f713fd4
Jeff Thompson <jeff.thompson51317@gmail.com>
2023-03-29 10:56:53 -0400

    Remove `req()` causing bug in sliders

--- R/mod_decision_automation.R
+++ R/mod_decision_automation.R
@@ -234,7 +234,7 @@ mod_decision_automation_server <- function(id, user){
           prev_lbl <- risk_lbl(decision_lst[.y - 1])
           
           observeEvent(input[[this_lbl]], {
-            if (req(.x %in% input$auto_include)) {
+            if (.x %in% input$auto_include) {
               auto_decision[[.x]] <- if (.y == 1) c(0, input[[this_lbl]]) else if (.y == length(decision_lst)) c(input[[this_lbl]], 1) else input[[this_lbl]]
             }
             
