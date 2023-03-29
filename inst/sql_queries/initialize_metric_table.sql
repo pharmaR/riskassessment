6d999b392985398e4eb77b8b0d8a81b8524653a9
Robert Krajcik <robert.krajcik@biogen.com>
2023-03-29 11:36:12 -0500

    put reverse_dependencies in maintenance metrics

--- inst/sql_queries/initialize_metric_table.sql
+++ inst/sql_queries/initialize_metric_table.sql
@@ -12,6 +12,6 @@ VALUES
 ('bugs_status',         'Bugs Closure Rate', '% of the last 30 bugs closed',     0, 1, 'maintenance', 1),
 ('license',             'License',           "Package's license",                0, 0, 'maintenance', 1),
 ('dependencies',        'Dependencies',      'Number of Package Dependencies',   0, 0, 'maintenance', 1),  
+('reverse_dependencies','Reverse Dependencies', 'Number of Reverse Dependencies', 0, 0, 'maintenance', 1),
 ('covr_coverage',       'Test Coverage',     'Percentage of objects tested',     0, 1, 'test', 0),
-('downloads_1yr',       'Downloads',         'Number of package downloads in the last year', 0, 0, 'community', 1),
-('reverse_dependencies','Reverse Dependencies', 'Number of Reverse Dependencies', 0, 0, 'community', 1);
+('downloads_1yr',       'Downloads',         'Number of package downloads in the last year', 0, 0, 'community', 1);
