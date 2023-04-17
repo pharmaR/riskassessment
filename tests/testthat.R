f64797a215a817de5b6eb49f3ee17ddeace559f9
Jeff Thompson <jeff.thompson51317@gmail.com>
2023-04-17 12:08:29 -0400

    Add `--no--sandbox` argument back in again...

--- tests/testthat.R
+++ tests/testthat.R
@@ -6,4 +6,11 @@ library(shinytest2)
 options(shinytest2.load_timeout = 30*1000)
 options(shinytest2.timeout = 30*1000)
 
+chromote::set_chrome_args(
+  c(
+    chromote::default_chrome_args(),
+    "--no-sandbox"
+  )
+)
+
 test_check("riskassessment")
