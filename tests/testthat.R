830137b1b64f17b8a679ac7aa9986de8cbd7e031
Jeff Thompson <jeff.thompson51317@gmail.com>
2023-04-17 09:29:23 -0400

    Instead of messing with `chromote`, specify loading of `shiny`

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,15 +1,9 @@
 library(testthat)
+library(shiny)
 library(riskassessment)
 # Load application support files into testing environment
 library(shinytest2)
 options(shinytest2.load_timeout = 30*1000)
 options(shinytest2.timeout = 30*1000)
-
-chromote::set_chrome_args(
-  c(
-    chromote::default_chrome_args(),
-    "--no-sandbox"
-  )
-)
 
 test_check("riskassessment")
