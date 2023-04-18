5a413472076608b52e185c1b03898d647acbbbcf
Jeff Thompson <jeff.thompson51317@gmail.com>
2023-04-18 13:15:11 -0400

    Fix reactivity of select input on changes and initialization

--- inst/app/www/js/role.js
+++ inst/app/www/js/role.js
@@ -18,20 +18,23 @@ $(document).on('shiny:connected', function() {
     let role_opts = ($(`#${role_lst.id}-admin`).is(':checked')) ? `${role_lst.role_opts.admin.map(createOption).join("")}` : `${role_lst.role_opts.nonadmin.map(createOption).join("")}`;
     // Replace text input with select input with appropriate options
     roleEl.replaceWith(`<select id="${role_lst.id}-role" class="${roleClass}">${role_opts}</select>`)
+    // Reconnect the appropriate shiny reactive with the select input
+    $(`#${role_lst.id}-role`).change(function() {
+     Shiny.setInputValue(`${role_lst.id}-role`, $(this).val());
+    })
     if (roleValue != '')
      $(`#${role_lst.id}-role`).val(roleValue);
-    // Reconnect the appropriate shiny reactive with the select input
-    $(`#${role_lst.id}-role`).change(function() {{
-     Shiny.setInputValue(`${role_lst.id}-role`, $(this).val());
-    }})
+    $(`#${role_lst.id}-role`).trigger("change");
     // Create observe to change options is user admin role changed.
-    $(`#${role_lst.id}-admin`).change(function() {{
-    if ($(this).is(':checked')) {{
+    $(`#${role_lst.id}-admin`).change(function() {
+    if ($(this).is(':checked')) {
       $(`#${role_lst.id}-role`).html(`${role_lst.role_opts.admin.map(createOption).join("")}`);
-    }} else {{
+      $(`#${role_lst.id}-role`).trigger("change");
+    } else {
       $(`#${role_lst.id}-role`).html(`${role_lst.role_opts.nonadmin.map(createOption).join("")}`);
-    }}
-    }})
+      $(`#${role_lst.id}-role`).trigger("change");
+    }
+    })
   })
 
 })
