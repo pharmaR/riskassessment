59a633d26ace8682fc08b6b771c097a3f681e049
Jeff Thompson <jeff.thompson51317@gmail.com>
2023-06-07 09:12:31 -0400

    Add audit log for comments table

--- inst/sql_queries/create_comments_table.sql
+++ inst/sql_queries/create_comments_table.sql
@@ -6,3 +6,64 @@ CREATE TABLE IF NOT EXISTS comments (
    comment_type CHAR,
    added_on 	 DATE
 );
+
+CREATE TABLE IF NOT EXISTS comments_audit_log (
+   id             INTEGER PRIMARY KEY AUTOINCREMENT,
+   comment_id     CHAR NOT NULL, 
+   old_row_data   JSON,
+   new_row_data   JSON,
+   dml_type       CHAR NOT NULL,
+   dml_timestamp  DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TRIGGER comments_update_audit_trigger
+AFTER UPDATE ON comments FOR EACH ROW
+BEGIN
+  INSERT INTO comments_audit_log (
+    comment_id,
+    old_row_data,
+    new_row_data,
+    dml_type
+  )
+  VALUES(
+    NEW.id,
+    JSON_OBJECT(
+      "user_name", OLD.user_name,
+      "user_role", OLD.user_role,
+      "comment", OLD.comment,
+      "comment_type", OLD.comment_type,
+      "added_on", OLD.added_on
+    ),
+    JSON_OBJECT(
+      "user_name", NEW.user_name,
+      "user_role", NEW.user_role,
+      "comment", NEW.comment,
+      "comment_type", NEW.comment_type,
+      "added_on", NEW.added_on
+    ),
+    'UPDATE'
+  );
+END
+
+CREATE TRIGGER comments_delete_audit_trigger
+AFTER DELETE ON comments FOR EACH ROW
+BEGIN
+  INSERT INTO comments_audit_log (
+    comment_id,
+    old_row_data,
+    new_row_data,
+    dml_type
+  )
+  VALUES(
+    OLD.id,
+    JSON_OBJECT(
+      "user_name", OLD.user_name,
+      "user_role", OLD.user_role,
+      "comment", OLD.comment,
+      "comment_type", OLD.comment_type,
+      "added_on", OLD.added_on
+    ),
+    NULL,
+    'DELETE'
+  );
+END
