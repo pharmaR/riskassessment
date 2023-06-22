6bde00dcb9e74f6598e57e980fd5e3d17e2633d8
AARON-CLARK <clark.aaronchris@gmail.com>
2023-06-22 13:01:40 -0400

    comment out url checker

--- app.R
+++ app.R
@@ -1,7 +1,7 @@
 # Launch the ShinyApp (Do not remove this comment)
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
-
+options(shiny.fullstacktrace = FALSE)
 pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
 options( "golem.app.prod" = TRUE)
 riskassessment::run_app(login_note = shiny::HTML(
