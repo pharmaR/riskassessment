033ac3c213640be7bf13277cf8426dddcec7629b
Jeff Thompson <jeff.thompson51317@gmail.com>
2023-06-29 15:48:52 -0400

    Remove contents of "source" directory on exit

--- R/app_server.R
+++ R/app_server.R
@@ -8,6 +8,11 @@
 #' @importFrom loggit loggit
 #' @noRd
 app_server <- function(input, output, session) {
+  
+  onStop(function() {
+    unlink("source/*", recursive = TRUE)
+    })
+  
   # Collect user info.
   user <- reactiveValues()
   credential_config <- get_golem_config("credentials", file = app_sys("db-config.yml"))
