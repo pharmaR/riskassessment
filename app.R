8ce5bcb22d55f9980f0d0335b6ed43ee0dc98a3c
Jeff Thompson <jeff.thompson51317@gmail.com>
2023-07-11 10:33:09 -0400

    Use configuration file for demo databases

--- app.R
+++ app.R
@@ -2,6 +2,7 @@
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
 
+Sys.setenv(GOLEM_ACTIVE_CONFIG = "demo")
 pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
 options( "golem.app.prod" = TRUE)
 riskassessment::run_app(
