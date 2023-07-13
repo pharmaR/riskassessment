eebb6038f6ad78dd69a5b4bf37741e039e9e5979
Robert Krajcik <robert.krajcik@biogen.com>
2023-07-13 10:09:11 -0400

    increase load_timeout value in testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,7 +3,7 @@ library(shiny)
 library(riskassessment)
 # Load application support files into testing environment
 library(shinytest2)
-options(shinytest2.load_timeout = 30*1000)
+options(shinytest2.load_timeout = 90*1000)
 options(shinytest2.timeout = 30*1000)
 
 chromote::set_chrome_args(
