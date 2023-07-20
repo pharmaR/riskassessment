7ea0d5b23343344d62220e93a6d863dfa5ef44b7
Jeff Thompson <jeff.thompson51317@gmail.com>
2023-07-20 08:48:46 -0400

    Allow specification of tarball directory for testing

--- R/utils.R
+++ R/utils.R
@@ -79,12 +79,13 @@ get_latest_pkg_info <- function(pkg_name) {
 #' @import dplyr
 #' 
 #' @noRd
-get_desc_pkg_info <- function(pkg_name, pkg_version) {
-  tar_file <- file.path(tempdir(), "tarballs", glue::glue("{pkg_name}_{pkg_version}.tar.gz"))
+get_desc_pkg_info <- function(pkg_name, pkg_version, tar_dir) {
+  if (missing(tar_dir)) tar_dir <- file.path(tempdir(), "tarballs")
+  tar_file <- file.path(tar_dir, glue::glue("{pkg_name}_{pkg_version}.tar.gz"))
   if (!file.exists(tar_file))
     return(get_latest_pkg_info(pkg_name))
   
-  untar_dir <- get_source(pkg_name, pkg_version)
+  untar_dir <- get_source(pkg_name, pkg_version, tar_dir = tar_dir)
   
   desc_file <- file.path(untar_dir, "DESCRIPTION")
   
@@ -575,12 +576,14 @@ get_tarball <- function(pkg_name, pkg_version, repo = "https://cran.rstudio.com"
   if (identical(class(out), "try-error")) out else file.path(out_dir, tarball)
 }
 
-get_source <- function(pkg_name, pkg_version) {
+get_source <- function(pkg_name, pkg_version, tar_dir) {
+  if (missing(tar_dir)) tar_dir <- file.path(tempdir(), "tarballs")
+  
   out_dir <- file.path(tempdir(), "source")
   source_dir <- file.path(out_dir, glue::glue("{pkg_name}_{pkg_version}"))
   if (dir.exists(file.path(source_dir, pkg_name))) return(file.path(source_dir, pkg_name))
   
-  tar_file <- file.path(tempdir(), "tarballs", glue::glue("{pkg_name}_{pkg_version}.tar.gz"))
+  tar_file <- file.path(tar_dir, glue::glue("{pkg_name}_{pkg_version}.tar.gz"))
   out <- try(untar(tar_file, exdir = source_dir), silent = TRUE)
   if (identical(class(out), "try-error")) out else file.path(source_dir, pkg_name)
 }
