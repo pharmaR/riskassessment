a2ec7f9784065aee2fe38144fa0eb6472f742584
Robert Krajcik <robert.krajcik@biogen.com>
2023-08-14 16:27:00 -0400

    update str_expect string for test_reportPreview.R

--- tests/testthat/test-reportPreview.R
+++ tests/testthat/test-reportPreview.R
@@ -61,7 +61,7 @@ test_that("Reactivity of reportPreview", {
     rvest::html_text() %>% 
     paste(collapse = ", ")
   
-  str_expect <- "Vignettes, NEWS file, NEWS current, Report Bugs, Website, Maintainer, Source Control, Documentation, Bugs Closure Rate, License, Test Coverage, First Version Release, Latest Version Release, Package Downloads"
+  str_expect <- "Vignettes, Report Bugs, Source Control, License, NEWS file, Website, Documentation, Test Coverage, NEWS current, Maintainer, Bugs Closure Rate, First Version Release*, Reverse Dependencies*, Latest Version Release*, Monthly downloads trend*, Package Downloads*"
   expect_equal(maint_info, str_expect)
   
   app$stop()
