90820cfaeb904e3991185dc4f4296a3f58edea9d
Jeff Thompson <jeff.thompson51317@gmail.com>
2023-08-30 14:22:28 -0400

    Remove old `makeNodes()`

--- R/mod_pkg_explorer_utils.R
+++ R/mod_pkg_explorer_utils.R
@@ -1,30 +1,3 @@
-# based on makeNodes() function in jsTreeR/inst/examples/folder/global.R
-#' @importFrom utils tail
-#' @noRd
-makeNodes <- function(leaves) {
-  dfs <- lapply(strsplit(leaves, "/"), function(s) {
-    item <- Reduce(function(a, b) paste0(a, "/", b), s[-1], s[1], accumulate = TRUE)
-    data.frame(item = item, parent = c("root", item[-length(item)]))
-  })
-  dat <- dfs[[1]]
-  for(i in 2:length(dfs)){
-    dat <- merge(dat, dfs[[i]], all = TRUE)
-  }
-  f <- function(parent){
-    i <- match(parent, dat$item)
-    item <- dat$item[i]
-    children <- dat$item[dat$parent == item]
-    label <- utils::tail(strsplit(item, "/")[[1]], 1)
-    if (length(children)) {
-      list(type = "root", text = label, data = item,
-           children = lapply(children, f))
-    } else {
-      list(type = "child", text = label, data = item)
-    }
-  }
-  lapply(dat$item[dat$parent == "root"], f)
-}
-
 #' Makes shinyTree Nodes
 #' 
 #' Creates nodes in the structure anticipated by shinyTree
