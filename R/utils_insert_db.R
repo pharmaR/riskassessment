6fb33ac49712d463e9d70a58e5e69e421e7a171d
Robert Krajcik <robert.krajcik@biogen.com>
2023-09-06 11:38:51 -0400

    use desc::desc_get_deps() instead of tryCatch for suggests

--- R/utils_insert_db.R
+++ R/utils_insert_db.R
@@ -146,7 +146,7 @@ upload_package_to_db <- function(name, version, title, description,
 #' @import dplyr
 #' @importFrom riskmetric pkg_ref pkg_assess pkg_score
 #' @importFrom glue glue 
-#' @importFrom desc desc_get_list
+#' @importFrom desc desc_get_deps desc_get_field
 #' @importFrom tools package_dependencies
 #' 
 #' @returns nothing
@@ -219,16 +219,13 @@ insert_riskmetric_to_db <- function(pkg_name,
   src_dir <- file.path("source", pkg_name)
   if (dir.exists(src_dir)) {
     desc_file <- glue::glue("source/{pkg_name}/DESCRIPTION")
-    tryCatch(
-      expr = {
-        sug_vctr <- desc::desc_get_list(key = 'Suggests', file = desc_file) %>% sort()
-      },
-      error = function(e) {
-        msg <- paste("Suggests not found for package", pkg_name)
-        rlang::warn(msg)
-        sug_vctr <- character(0)
-      }
-    )
+    if ('Suggests' %in% desc::desc_get_deps(file = desc_file)) {
+    sug_vctr <- desc::desc_get_field(key = 'Suggests', file = desc_file) %>% sort()
+    } else {
+      msg <- paste("Suggests not found for package", pkg_name)
+      rlang::warn(msg)
+      sug_vctr <- character(0)
+    }
   } else {
     sug_vctr <- unlist(tools::package_dependencies(pkg_name, available.packages(contrib.url(repos = "http://cran.us.r-project.org")),
                        which=c("Suggests"), recursive=FALSE)) %>% unname() %>% sort()
