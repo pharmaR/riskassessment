bc4bd5484318e704966b7f870c96f6c5ff43740a
AARON-CLARK <clark.aaronchris@gmail.com>
2023-09-20 13:18:39 -0400

    fixed broken test

--- tests/testthat/test-metric_gauge.R
+++ tests/testthat/test-metric_gauge.R
@@ -14,15 +14,19 @@ meter_val <- function(x) {
   ))
 }
 
-test_that("metric_gauge() working...", {
-  expect_equal(class(metric_gauge("NA")), "shiny.tag.list")
+test_that("metric_gauge() 'Label' tag working...", {
+  expect_equal(class(metric_gauge("NA")), c("shiny.tag.list", "list"))
   
   expect_equal(metric_gauge("NA") |> my_attr("label") ,"NA")
+  expect_equal(metric_gauge("0") |> my_attr("label") |> substr(1,1),"1")
   expect_equal(metric_gauge(".25") |> my_attr("label") ,"0.75")
   expect_equal(metric_gauge(".45") |> my_attr("label") ,"0.55")
   expect_equal(metric_gauge("1") |> my_attr("label") |> substr(1,1),"0")
- 
+})
+test_that("metric_gauge() 'value' working...", {
   expect_equal(metric_gauge("NA") |> meter_val() ,"0")
+  expect_equal(metric_gauge("0") |> meter_val(),"0.08")
+  expect_equal(metric_gauge(".06") |> meter_val(),"0.08")
   expect_equal(metric_gauge(".25") |> meter_val() ,"0.25")
   expect_equal(metric_gauge(".45") |> meter_val() ,"0.45")
   expect_equal(metric_gauge("1") |> meter_val(), "1")
