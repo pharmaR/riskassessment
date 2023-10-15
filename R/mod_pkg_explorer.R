5286f190f0c714aeb243edc33144b5b11b452ef5
narayanan-iyer-pfizer <114721252+narayanan-iyer-pfizer@users.noreply.github.com>
2023-10-15 19:23:29 +0530

    Add 581 introjs button and autoselect first
    add introjs button for both package explorer and code explorer,make the first element  get selected and explanded automatically for package explorer file tree

--- R/mod_pkg_explorer.R
+++ R/mod_pkg_explorer.R
@@ -38,10 +38,11 @@ mod_pkg_explorer_server <- function(id, selected_pkg,
       } else if (!file.exists(file.path("tarballs", glue::glue("{selected_pkg$name()}_{selected_pkg$version()}.tar.gz")))) {
         showHelperMessage(message = glue::glue("Source code not available for {{{selected_pkg$name()}}}"))
       } else {
-        div(
+        div(introJSUI(NS(id, "introJS")),
             br(),
             fluidRow(
               column(4,
+                     div(id = "file_tree",
                      wellPanel(
                        {
                          treeTag <- 
@@ -49,9 +50,10 @@ mod_pkg_explorer_server <- function(id, selected_pkg,
                          treeTag[[1]]$children[[3]] <- NULL
                          treeTag
                        }
-                     )
+                     ))
               ),
               column(8,
+                     div(id = "file_editor",
                      conditionalPanel(
                        condition = "output.is_file",
                        shinyAce::aceEditor(ns("editor"), value = "", height = "62vh",
@@ -61,11 +63,11 @@ mod_pkg_explorer_server <- function(id, selected_pkg,
                        ),
                        htmlOutput(ns("filepath")),
                        ns = ns
-                     )
+                     ))
               )
             ),
             br(), br(),
-            div(id = ns("comments_for_se"), fluidRow(
+            div(id = "comments_for_se", fluidRow(
               if ("general_comment" %in% credentials$privileges[[user$role]]) addCommentUI(id = ns("add_comment")),
               viewCommentsUI(id = ns("view_comments")))),
           id = id
@@ -83,7 +85,10 @@ mod_pkg_explorer_server <- function(id, selected_pkg,
 
     nodes <- reactive({
       req(pkgdir())
-      make_nodes(list.files(pkgdir(), recursive = TRUE))
+      s <- make_nodes(list.files(pkgdir(), recursive = TRUE)) 
+      attr(s[[1]][[1]],"stselected") = TRUE
+      attr(s[[1]],"stopened") = TRUE
+      s
     }) %>%
       bindEvent(pkgdir(), selected_pkg$name())
     
@@ -116,6 +121,8 @@ mod_pkg_explorer_server <- function(id, selected_pkg,
       }
       shinyAce::updateAceEditor(session, "editor", value = s, mode = e)
     })
+    
+    introJSServer("introJS", text = reactive(pe_steps), user, credentials)
     
     output$filepath <- renderUI({
       s <- if (is_file())
