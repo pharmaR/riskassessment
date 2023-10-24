5ef937a70cc2b224977039a2362adfa007a42ab9
AARON-CLARK <clark.aaronchris@gmail.com>
2023-10-24 13:29:54 -0400

    update database with other half of 300 pkgs, and rewrite login_note

--- app.R
+++ app.R
@@ -3,21 +3,20 @@
 # Or use the blue button on top of this file
 
 Sys.setenv(GOLEM_CONFIG_ACTIVE = "demo")
+options(rsconnect.max.bundle.files = 20000)
 options(shiny.fullstacktrace = TRUE)
 pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
 options( "golem.app.prod" = TRUE)
 riskassessment::run_app(
   login_note = shiny::HTML(
   "<em>Note:</em><br>
-  To run as ADMIN, type username 
-  <span style='text-transform:none;'><u>demo_admin</u></span> with password <u><span style='text-transform:none;'>Admin@1</span></u> above.
+  Use the following credentials to log on with varying roles & privileges in the app, expanded upon <a href='https://pharmar.github.io/riskassessment/articles/User_Roles_and_Privileges.html#roles' target='_blank'>here</a>:
   <br><br>
-  To run as LEAD, type username
-    <span style='text-transform:none;'><u>demo_lead</u></span> with password <u><span style='text-transform:none;'>Lead@1</span></u> above.
-  <br><br>
-  To run as REVIEWER, type username
-    <span style='text-transform:none;'><u>demo_reviewer</u></span> with password <u><span style='text-transform:none;'>Reviewer@1</span></u> above.
-    <br><br>
-  To run as VIEWER, type username
-    <span style='text-transform:none;'><u>demo_viewer</u></span> with password <u><span style='text-transform:none;'>Viewer@1</span></u> above.")
+    U: <span style='text-transform:none;'><u>demo_admin</u></span>; &nbsp;&nbsp;&nbsp; P: <u><span style='text-transform:none;'>Admin@1</span></u>
+  <br>
+    U: <span style='text-transform:none;'><u>demo_lead</u></span>; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; P: <u><span style='text-transform:none;'>Lead@1</span></u>
+  <br>
+    U: <span style='text-transform:none;'><u>demo_reviewer</u></span>; P: <u><span style='text-transform:none;'>Reviewer@1</span></u>
+    <br>
+    U: <span style='text-transform:none;'><u>demo_viewer</u></span>; &nbsp;&nbsp;  P: <u><span style='text-transform:none;'>Viewer@1</span></u>")
   ) 
