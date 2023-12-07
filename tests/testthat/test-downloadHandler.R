63983e54e5cb7733b9ba89232acd07d6ec850711
Jeff Thompson <jeff.thompson51317@gmail.com>
2023-12-07 12:16:05 -0500

    Add PDF downloads into test

--- tests/testthat/test-downloadHandler.R
+++ tests/testthat/test-downloadHandler.R
@@ -12,9 +12,9 @@ test_that("downloadHandler works", {
   report <- app$get_download("downloadHandler_1-download_reports")
   expect_equal(tools::file_ext(report), "docx")
   # 
-  # app$set_inputs(`downloadHandler_1-report_format` = "pdf")
-  # report <- app$get_download("downloadHandler_1-download_reports")
-  # expect_equal(tools::file_ext(report), "pdf")
+  app$set_inputs(`downloadHandler_1-report_format` = "pdf")
+  report <- app$get_download("downloadHandler_1-download_reports")
+  expect_equal(tools::file_ext(report), "pdf")
   
   app$set_inputs(tabs = "multiple")
   expect_equal(app$get_value(input = "downloadHandler_2-report_format"), "html")
