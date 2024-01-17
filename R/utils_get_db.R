c5e19b449fb5359337ccbea5889ae80450355eaf
Jeff Thompson <jeff.thompson51317@gmail.com>
2024-01-17 09:57:55 -0500

    Stop initial crash for packages with dependency error from riskmetric

--- R/utils_get_db.R
+++ R/utils_get_db.R
@@ -229,10 +229,10 @@ get_metric_data <- function(pkg_name, metric_class = 'maintenance', db_name = go
 #' @noRd
 get_depends_data <- function(pkg_name, db_name = golem::get_golem_options('assessment_db_name')){
   
-pkgref <- get_assess_blob(pkg_name, db_name)
-
-if(suppressWarnings(is.null(pkgref$dependencies[[1]]))) {
-  dplyr::tibble(package = character(0), type = character(0), name = character(0))
+  pkgref <- get_assess_blob(pkg_name, db_name)
+  
+  if(suppressWarnings(is.null(nrow(pkgref$dependencies[[1]])) || nrow(pkgref$dependencies[[1]]) == 0)) {
+    dplyr::tibble(package = character(0), type = character(0), name = character(0))
   } else {
     pkgref$dependencies[[1]] %>% dplyr::as_tibble() %>% 
       mutate(package = stringr::str_replace(package, "\n", " ")) %>%
