7e2354e6a66b5a1e83a553c8f29dbf6c9f0c08d4
Aaron Clark (Arcus) <aclark02@arcusbio.com>
2024-04-11 14:52:23 -0400

    Add a few more logos and then jazzed up the Contact page

--- R/mod_aboutInfo.R
+++ R/mod_aboutInfo.R
@@ -29,13 +29,13 @@ aboutInfoUI <- function(id) {
             # contacts description.
             br(),
             shiny::HTML(
-               "<h3> Report Issues </h3>
+               "<h3><i class='fa fa-github' aria-hidden='true'></i> Report Issues </h3>
                 <p> To propose new features or report a bug, please review our open issues first on our 
                 <a tagret='_blank' href='https://github.com/pharmaR/riskassessment/issues' > GitHub page</a>. If your topic hasn't been addressed, we highly encourage you to <a tagret='_blank' href='https://github.com/pharmaR/riskassessment/issues/new' >open a new issue</a>!</p>
                 <br>
                 <br>
                 
-                <h3> Developemnt Team </h3> 
+                <h3><i class='fa fa-coffee' aria-hidden='true'></i> Developemnt Team </h3> 
                 <p> Most all inquiries should flow through the GitHub page reference above (start there), but you may reach out to the lead developer at any time, especially if you'd like to start contributing to the project!</p>
                 <br>
                 <ul>  
@@ -43,7 +43,7 @@ aboutInfoUI <- function(id) {
                 </ul> 
                 <br>                           
                 
-                <h3> R Validation Hub </h3>
+                <h3><img src='www/R_validation_hub_logo.png' alt='R Validation Hub Logo' style = 'height:50px;padding-right:8px;'>R Validation Hub </h3>
                 <p> To learn more about the work and initiatives of R Validation Hub, please visit <a target='_blank' href= 'https://www.pharmar.org/about/'> 
                                             https://www.pharmar.org/about/</a>. </p>  ")
           ),
@@ -56,13 +56,13 @@ aboutInfoUI <- function(id) {
                "<h3> Official Workstream of the <a target='_blank' href='https://www.pharmar.org/'>R Validation Hub</a>.
                 <br/>Sponsored by the <a target='_blank' href='https://www.r-consortium.org//'>R Consortium</a></h3>
                 <br>
-                <img src='www/pharmaRlogo_large.png' alt='R Validation Hub Logo' style = 'height:100px;padding-right: 50px;'>  
-                <br>
-                <img src='www/rc_logo.png' alt = 'ProCogia logo' style='height:100px;'> 
+                <img src='www/pharmaRlogo_large.png' alt='R Validation Hub Logo' style = 'height:130px;padding-right:50px;padding-top:15px;padding-bottom:15px;'>  
+                <img src='www/rc_logo.png' alt = 'ProCogia logo' style='height:130px;padding-top:15px;padding-bottom:15px;'> 
                 <br>
                 
                 <br>
                 <h3> Collaborative Deployment hosted by <a target='_blank' href='https://procogia.com/'> ProCogia </a> </h3>
+                <br>
                 <img src='www/procogia_logo.png' alt = 'ProCogia logo' style='height:100px'> 
                 <br>
                 
@@ -94,16 +94,22 @@ aboutInfoUI <- function(id) {
                 
                 <h3> Contributing Companies </h3>
                 
-                <img src='www/appsilon_logo.jpg' alt='Appsilon' style = 'height:100px;padding-right:40px;'> 
-                <img src='www/arcus_logo.png' alt='Arcus Biosciences' style = 'height:100px;padding-right:40px;'> 
-                <img src='www/bi_logo.png' alt='Boehringer Ingelheim' style = 'height:100px;padding-right:40px;'>
-                <img src='www/biogen_logo.jpg' alt='Biogen' style = 'height:100px;padding-right:40px;'>
-                <img src='www/cytel.png' alt='Cytel' style = 'height:100px;padding-right:40px;'>
-                <img src='www/fission_logo.png' alt='Fission Labs India Pvt Ltd' style = 'height:100px;padding-right:40px;'>
-                <img src='www/gcp_logo.png' alt='GCP-Service International' style = 'height:100px;padding-right:40px;'>
-                <img src='www/GSK_logo.jpg' alt='GSK' style = 'height:100px;padding-right:40px;'>
-                <img src='www/katalyzedata_logo.jpg' alt='Katalyze Data' style = 'height:100px;padding-right:40px;'>
-                <img src='www/pfizer_logo.png' alt='Pfizer' style = 'height:100px'>
+                <img src='www/appsilon_logo.jpg' alt='Appsilon' style = 'height:100px;padding-right:40px;padding-top:15px;padding-bottom:15px;'> 
+                <img src='www/arcus_logo.png' alt='Arcus Biosciences' style = 'height:100px;padding-right:40px;padding-top:15px;padding-bottom:15px;'> 
+                <img src='www/bi_logo.png' alt='Boehringer Ingelheim' style = 'height:100px;padding-right:40px;padding-top:15px;padding-bottom:15px;'>
+                <img src='www/biogen_logo.jpg' alt='Biogen' style = 'height:100px;padding-right:40px;padding-top:15px;padding-bottom:15px;'>
+                <img src='www/cytel.png' alt='Cytel' style = 'height:100px;padding-right:40px;padding-top:15px;padding-bottom:15px;'>
+                <img src='www/fission_logo.png' alt='Fission Labs India Pvt Ltd' style = 'height:100px;padding-right:40px;padding-top:15px;padding-bottom:15px;'>
+                <img src='www/gcp_logo.png' alt='GCP-Service International' style = 'height:100px;padding-right:40px;padding-top:15px;padding-bottom:15px;'>
+                <img src='www/GSK_logo.jpg' alt='GSK' style = 'height:100px;padding-right:40px;padding-top:15px;padding-bottom:15px;'>
+                <img src='www/katalyzedata_logo.jpg' alt='Katalyze Data' style = 'height:100px;padding-right:40px;padding-top:15px;padding-bottom:15px;'>
+                <img src='www/merck_logo.png' alt='Merck' style = 'height:100px;padding-top:15px;padding-bottom:15px;'>
+                <img src='www/pfizer_logo.png' alt='Pfizer' style = 'height:100px;padding-top:15px;padding-bottom:15px;'>
+                <img src='www/roche_logo.png' alt='Roche' style = 'height:100px;padding-top:15px;padding-bottom:15px;'>
+                <br>
+                <br>
+                <br>
+                <br>
                ")
           )
         )
