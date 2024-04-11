9b1c6f3e27e8b8baf32f6d4a049834e2e7e39fb8
Aaron Clark (Arcus) <aclark02@arcusbio.com>
2024-04-11 12:06:09 -0400

    Added procogia logo, updated description.

--- R/mod_aboutInfo.R
+++ R/mod_aboutInfo.R
@@ -11,29 +11,88 @@ aboutInfoUI <- function(id) {
     fluidRow(
       column(
         width = 8, offset = 2,
-        h2("About Risk Assessment Application", align = "center", `padding-bottom`="20px"),
+        h2("About", align = "center", `padding-bottom`="20px"),
         br(),
-        
+        br(),
         # Assessment criteria sub-tab
         tabsetPanel(
           tabPanel(
             title = "Assessment Criteria",
             icon = icon("circle-info"),
-            assessmentInfoUI("assessmentInfo")  # call assessment module UI
+            br(),
+            assessmentInfoUI(NS(id,"assessmentInfo"))  # call assessment module UI
                ),
         # Contacts sub-tab  
           tabPanel(
             title = "Contact",
-            h6("List of Important Contacts"),
+            icon = icon("envelope"),
+            # contacts description.
             br(),
-            uiOutput(NS(id,"contact_text")),  # ui output with contacts description.
+            shiny::HTML(
+               "<h3> Report Issues </h3>
+                <p> To propose new features or report a bug, please review our open issues first on our 
+                <a tagret='_blank' href='https://github.com/pharmaR/riskassessment/issues' > GitHub page</a>. If your topic hasn't been addressed, we highly encourage you to <a tagret='_blank' href='https://github.com/pharmaR/riskassessment/issues/new' >open a new issue</a>!</p>
+                <br>
+                <br>
+                
+                <h3> Developemnt Team </h3> 
+                <p> Most all inquiries should flow through the GitHub page reference above (start there), but you may reach out to the lead developer at any time, especially if you'd like to start contributing to the project!</p>
+                <br>
+                <ul>  
+                  <li>Aaron Clark [ <a href= 'mailto:clark.aaronchris@gmail.com'> Send Email </a> ] </li>
+                </ul> 
+                <br>                           
+                
+                <h3> R Validation Hub </h3>
+                <p> To learn more about the work and initiatives of R Validation Hub, please visit <a target='_blank' href= 'https://www.pharmar.org/about/'> 
+                                            https://www.pharmar.org/about/</a>. </p>  ")
           ),
         # Contributors sub-tab
           tabPanel(
             title = "Contributors and Companies",
-            h6("List of Contributors and Comanies"),
+            icon = icon("address-card"),
             br(),
-            uiOutput(NS(id,"contributor_text")), # ui output with contributors and companies description.
+            shiny::HTML(
+               "<h3> Collaborative Deployment hosted by <a target='_blank' href='https://procogia.com/'> ProCogia </a> </h3>
+                <img src='www/procogia_logo.png' alt = 'ProCogia logo' style='height:100px'> 
+                <br>
+                
+                <br>
+                <h3> Current Contributors and Companies</h3>
+                <ul>
+                  <li> Aaron Clark </li>
+                  <li> Jeff Thompson </li>
+                  <li> Robert Krajcik </li>
+                  <li> Narayanan Iyer </li>
+                  <li> R Validation Hub </li>
+                </ul>
+                
+                <br>
+                
+                <h3> Past Contributors and Companies </h3>
+                
+                <ul>
+                  <li> Lars Andersen </li>
+                  <li> Andrew Borgman </li>
+                  <li> Scott Schumacker </li>
+                  <li> Marly Gotti </li>
+                  <li> Maya Gans </li>
+                  <li> Aravid Reddy Kallem </li>
+                  <li> Eduardo Almeida </li>
+                  <li> Munshi Imran Hossain </li>
+                  <li> Fission Labs India Pvt Ltd </li>
+                  <li> GSK </li>
+                  <li> Biogen</li>
+                  <li> Appsilon</li>
+                </ul>
+                <br>
+                
+                <img src='www/R_validation_hub_logo.png' alt='R Validation Hub Logo' style = 'height:100px'>
+                <img src='www/fission_logo.png' alt='Fission Labs India Pvt Ltd' style = 'height:100px'>
+                <img src='www/GSK_logo.jpg' alt='GSK' style = 'height:100px'>
+                <img src='www/biogen_logo.jpg' alt='Biogen' style = 'height:100px'>
+                <img src='www/appsilon_logo.jpg' alt='Appsilon' style = height:80px'> "
+               )
           )
         )
       )))
@@ -48,13 +107,6 @@ aboutInfoUI <- function(id) {
 #' 
 aboutInfoServer <- function(id, metric_weights) {
   moduleServer(id, function(input, output, session) {
-    
-    # Display the contact description.
-
-    output$contact_text <- renderUI({contact_text})
-
-    # Display the contributor description.
-    output$contributor_text <- renderUI({contributor_text})
     
     # Load server of the assessment criteria module.
     assessmentInfoServer("assessmentInfo", metric_weights = metric_weights) 
