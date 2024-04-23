35922b9155f5997e7a6721eb6a4b5b48b5813d46
Jeff Thompson <160783290+jthompson-arcus@users.noreply.github.com>
2024-04-23 10:35:16 -0400

    Implement a waiter for code explorer

--- tests/testthat/test-code_explorer.R
+++ tests/testthat/test-code_explorer.R
@@ -29,6 +29,7 @@ test_that("pkg_explorer works", {
   app <- shinytest2::AppDriver$new(test_path("test-apps", "explorer-app"))
   
   app$set_inputs(tabs = "fn_expl_tab")
+  app$wait_for_value(input = "fn_explorer-test_files")
 
   expect_equal(
     app$get_values(input = paste("fn_explorer", c("exported_function", "file_type"), sep = "-"))$input,
