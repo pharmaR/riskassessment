6525815de5a23fd9eaf5d56b8734aef01978b605
Aaron Clark (Arcus) <aclark02@arcusbio.com>
2024-05-06 09:53:45 -0400

    udpate risk score legend modal to include footer, and hpyerlink to riskmetric::pkg_ref, in addition to other small grammar fixes

--- R/mod_legend.R
+++ R/mod_legend.R
@@ -28,7 +28,7 @@ legendUI <- function(id){
     
 #' legend Server Functions
 #'
-#' @importFrom shiny showModal modalDialog
+#' @importFrom shiny showModal modalDialog HTML
 #' @noRd 
 legendServer <- function(id){
   moduleServer( id, function(input, output, session){
@@ -38,27 +38,36 @@ legendServer <- function(id){
         size = "l",
         easyClose = TRUE,
         title = h3("Risk Score Legend"),
+        footer = tagList(
+          # icon('circle-info'),
+          HTML("<span><i class='fas fa-circle-info'></i> The displayed assessment-level scores are inverted from the {riskmetric} package to maintain a consistent interpretation with the overall package score, where 0 indidcates low risk and 1 indicates high risk.</span>"),
+          modalButton("Dismiss")),
         tagList(
           br(),
           div(style = "display: flex; align-items: center;",
             icon("triangle-exclamation", class = "text-warning", verify_fa = FALSE, style = "font-size:65px; padding-left: 10px;"),
-            div(style = "position: absolute; left: 8rem; padding-right: 20px;", "A 'Not Found' indicates there was no assessment value derived from {riskmetric}'s fetch for this information. This is usually due to the reference sourced used when loading the package into the database. For example, {riskmetric} cannot fetch testing coverage when using the CRAN Remote reference source. Since there is no assessment value, there is also no score. As such, this score will not impact the package's overall risk score.")),
+            div(style = "position: absolute; left: 8rem; padding-right: 20px;",
+                HTML("A 'Not Found' indicates there was no assessment value derived from {riskmetric}'s fetch for this information. This is usually due to the reference sourced used when loading the package into the database. See <a href='https://pharmar.github.io/riskmetric/reference/pkg_ref.html#details' target='_blank'><code>riskmetric::pkg_ref</code></a> for more information. Since there is no assessment value, there is also no score. As such, this score will NOT impact the package's overall risk score."))),
           br(),br(),br(),
           div(style = "display: flex; align-items: center;",
               metric_gauge("NA"),
-              div(style = "position: absolute; left: 8rem; padding-right: 20px;", "An 'NA' indicates an assessment value exists for {riskmetric}, but there is no score recorded. This is usually an intentional practice performed by {riskmetric}'s development team when scoring such a metric is ambiguous or strange, thus a score is not applicable")),
+              div(style = "position: absolute; left: 8rem; padding-right: 20px;",
+                  "An 'NA' indicates an assessment value exists in the {riskmetric} output, but there is no score recorded. This is usually an intentional practice performed by {riskmetric}'s development team when scoring a metric is ambiguous or strange, thus a score is not applicable. This score will NOT impact the package's overall risk score.")),
           br(),
           div(style = "display: flex; align-items: center;",
               metric_gauge("0"),
-              div(style = "position: absolute; left: 8rem; padding-right: 20px;","A score of 1 indicates the highest risk possible based on the {riskmetric} assessment value.")),
+              div(style = "position: absolute; left: 8rem; padding-right: 20px;",
+                  "A score of 1 indicates the highest risk possible, based on the {riskmetric}-derived assessment value.")),
           br(),
           div(style = "display: flex; align-items: center;",
               metric_gauge(".50"),
-              div(style = "position: absolute; left: 8rem; padding-right: 20px;", "A score close to 1 indicates the {riskmetric} assessment value is high risk, where scores closer to 0 inidicate lower risk")),
+              div(style = "position: absolute; left: 8rem; padding-right: 20px;",
+                  "A score close to 1 indicates that the {riskmetric} assessment value is high risk, where a score closer to 0 inidicates lower risk")),
           br(),
           div(style = "display: flex; align-items: center;",
               metric_gauge("1"),
-              div(style = "position: absolute; left: 8rem; padding-right: 20px;", "A score of 0 indicates the lowest possible risk based on the {riskmetric} assessment value."))
+              div(style = "position: absolute; left: 8rem; padding-right: 20px;",
+                  "A score of 0 indicates the lowest possible risk, based on the {riskmetric}-derived assessment value."))
         )
       ))
     })
