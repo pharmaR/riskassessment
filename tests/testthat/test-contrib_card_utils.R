082f15a14b144a793208812cfad7284e9f932147
Aaron Clark (Arcus) <aclark02@arcusbio.com>
2024-05-20 11:49:19 -0400

    make a usable test for contrib cards and related artifacts

--- tests/testthat/test-contrib_card_utils.R
+++ tests/testthat/test-contrib_card_utils.R
@@ -1,14 +1,14 @@
 
 
 # First, make sure data frame has the columns we expect
-test_that("team_info_df is in expected format", {
+test_that("team_info_df has the 'most crucial' columns", {
   
   testthat::expect_true(all(c("name", "role", "site", "org", "photo_file") %in% names(team_info_df)))
   
 })
 
 # Make sure the png's are squares
-test_that("Pngs formatted correctly", {
+test_that("Pngs formatted as squares", {
   
   if(rlang::is_installed("png")) {
     purrr::walk(team_info_df$photo_file, function(x){
@@ -20,26 +20,18 @@ test_that("Pngs formatted correctly", {
 })
 
 
-test_that("contrib_card() works", {
+test_that("contrib_card() produces bslib shiny.tag", {
 
-  # # custom
-  # contrib_card(
-  #   role = "Workstream Lead",
-  #   pic = 'inst/app/www/images/aaron_clark.png',
-  #   site = 'https://github.com/aclark02-arcus',
-  #   name = "Aaron Clark",
-  #   org = "Arcus Biosciences")
-  # 
-  # # based on team_info_Df
-  # team_member1 <- contrib_card(
-  #   role = team_info_df$role[1],
-  #   pic = NULL,
-  #   site = team_info_df$site[1],
-  #   name = team_info_df$name[1],
-  #   org = team_info_df$org[1])
-  # team_member1
-  # class(team_member1)  
-  # attributes(team_member1)
+  # custom
+  team_member1 <- contrib_card(
+    role = "Workstream guy",
+    pic = NULL,
+    site = 'https://github.com/pharmar/riskassessment',
+    name = "Some Guy",
+    org = "Some Company")
+
+  testthat::expect_equal(class(team_member1),
+                         c("bslib_fragment","shiny.tag"))
   
 })
 
@@ -47,14 +39,12 @@ test_that("contrib_card() works", {
 
 test_that("make_contrib_cards() works", {
   
-  # make_contrib_cards(
-  #   team_info_df |>
-  #      filter(status %in% "current") 
-  #   )
-  # 
-  # make_contrib_cards(team_info_df %>% filter(status %in% "past"))
-  # class(team_member1)  
-  # attributes(team_member1)
+  if(all(c("name", "role", "site", "org", "photo_file") %in% names(team_info_df))) {
+
+    past <- make_contrib_cards(team_info_df %>% filter(status %in% "past"))
+    testthat::expect_equal(class(past),
+                           c("bslib_fragment","shiny.tag"))
+  }
   
 })
 
